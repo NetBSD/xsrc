0456a307fe7f4822417bc7bc376606e08d2774a4
mrg <mrg@NetBSD.org>
1997-06-30 14:22:53 +0000

    XFree86 3.3 sources.

--- xc/util/compress/compress.c
+++ xc/util/compress/compress.c
@@ -169,11 +169,8 @@ char_type magic_header[] = { "\037\235" };	/* 1F 9D */
  *		James A. Woods		(decvax!ihnp4!ames!jaw)
  *		Joe Orost		(decvax!vax135!petsd!joe)
  *
- * $Header: /cvsroot/xsrc/xc/util/compress/Attic/compress.c,v 1.1.1.1 1997/03/15 06:19:30 scottr Exp $
- * $Log: compress.c,v $
- * Revision 1.1.1.1  1997/03/15 06:19:30  scottr
- * XFree86 3.2 sources
- *
+ * Header: compress.c,v 4.0 85/07/30 12:50:00 joe Release
+ * Log:	compress.c,v
  * Revision 4.0  85/07/30  12:50:00  joe
  * Removed ferror() calls in output routine on every output except first.
  * Prepared for release to the world.
@@ -284,7 +281,7 @@ char_type magic_header[] = { "\037\235" };	/* 1F 9D */
  * Add variable bit length output.
  *
  */
-static char rcs_ident[] = "$Header: /cvsroot/xsrc/xc/util/compress/Attic/compress.c,v 1.1.1.1 1997/03/15 06:19:30 scottr Exp $";
+static char rcs_ident[] = "Header: compress.c,v 4.0 85/07/30 12:50:00 joe Release";
 
 #include <stdio.h>
 #include <ctype.h>
