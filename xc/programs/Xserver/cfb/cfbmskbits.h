e3aea98c51d0ac7c5bb53655d45f04de8c1d22a2
simonb <simonb>
1999-04-19 01:55:23 +0000

    Pull in .../compiler.h if on a mips as well to get ldq_u().

--- xc/programs/Xserver/cfb/cfbmskbits.h
+++ xc/programs/Xserver/cfb/cfbmskbits.h
@@ -45,7 +45,8 @@ THE USE OR PERFORMANCE OF THIS SOFTWARE.
  * XXX about the possibility of using this code on an Alpha running
  * XXX something other than Linux.
  */
-#if defined(__NetBSD__) && (defined(__alpha__) || defined(__vax__))
+#if defined(__NetBSD__) && \
+	(defined(__alpha__) || defined(__vax__) || defined(__mips__))
 #include	"../hw/xfree86/common/compiler.h"
 #endif
 
