b2a3b6a25830c3cf1ccd472df389b02cee209484
tron <tron@NetBSD.org>
2001-04-10 10:30:10 +0000

    "/etc/rc.d/xdm" wants "xdm.pid" not "xdm-pid".

--- xfree/xc/programs/xdm/config/xdm-conf.cpp
+++ xfree/xc/programs/xdm/config/xdm-conf.cpp
@@ -1,7 +1,7 @@
 ! $XConsortium: xdm-conf.cpp /main/3 1996/01/15 15:17:26 gildea $
 ! $XFree86: xc/programs/xdm/config/xdm-conf.cpp,v 1.5 1999/08/29 12:21:06 dawes Exp $
 DisplayManager.errorLogFile:	XDMLOGDIR/xdm-errors
-DisplayManager.pidFile:		XDMPIDDIR/xdm-pid
+DisplayManager.pidFile:		XDMPIDDIR/xdm.pid
 DisplayManager.keyFile:		XDMDIR/xdm-keys
 DisplayManager.servers:		XDMDIR/Xservers
 DisplayManager.accessFile:	XDMDIR/Xaccess
