a15b6c7e9ed564e52eafab36dfe7eeaa24086cba
tron <tron@NetBSD.org>
2001-05-11 21:09:04 +0000

    Add support for "WSMouse" mouse protocoll.

--- xfree/xc/programs/Xserver/hw/xfree86/xf86config/xf86config.c
+++ xfree/xc/programs/Xserver/hw/xfree86/xf86config/xf86config.c
@@ -337,6 +337,9 @@ static char *mousetype_identifier[] = {
 #if defined(__EMX__) || defined(QNX4)
 	"OSMOUSE"
 #endif
+#if defined(__NetBSD__)
+	"WSMouse"
+#endif
 };
 
 #ifndef __EMX__
@@ -353,7 +356,12 @@ static char *mousetype_name[] = {
 	"Logitech MouseMan (Microsoft compatible)",
 	"MM Series",	/* XXXX These descriptions should be improved. */
 	"MM HitTablet",
+#ifdef	__NetBSD__
+	"Microsoft IntelliMouse",
+	"NetBSD WSCONS Mouse"
+#else
 	"Microsoft IntelliMouse"
+#endif
 };
 
 static char *mousedev_text =
@@ -410,7 +418,11 @@ mouse_configuration(void) {
 	char s[80];
 	printf("%s", mouseintro_text);
 	
+#ifdef __NetBSD__
+	for (i = 0; i < 10; i++)
+#else
 	for (i = 0; i < 9; i++)
+#endif
 		printf("%2d.  %s\n", i + 1, mousetype_name[i]);
 
 	printf("\n");
