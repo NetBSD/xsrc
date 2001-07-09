a23cf4517b08415d21f82eb2a1ec6a8a7540df14
tron <tron@NetBSD.org>
2001-07-09 06:14:20 +0000

    Restore nVidia specific and vgaHW registers in the correct order. Fix supplied by Matthieu Herrb in private e-mail. This should fix PR xsrc/13155.

--- xfree/xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h
+++ xfree/xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h
@@ -1,4 +1,4 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h,v 1.4 2000/10/24 22:45:08 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/nv/nv_const.h,v 1.4.4.1 2001/07/08 22:21:38 herrb Exp $ */
 
 #ifndef __NV_CONST_H__
 #define __NV_CONST_H__
@@ -8,7 +8,7 @@
 #define NV_DRIVER_NAME "nv"
 #define NV_MAJOR_VERSION 1
 #define NV_MINOR_VERSION 0
-#define NV_PATCHLEVEL 0
+#define NV_PATCHLEVEL 1
 
 #define NV_USE_FB
 
