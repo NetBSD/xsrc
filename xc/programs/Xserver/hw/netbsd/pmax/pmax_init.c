0a1aeecf9367748fafc280a7fc9b8ed3cd7cd4e2
thorpej <thorpej>
2001-09-19 20:52:27 +0000

    Adjust to fbio.h include file changes.

--- xc/programs/Xserver/hw/netbsd/pmax/pmax_init.c
+++ xc/programs/Xserver/hw/netbsd/pmax/pmax_init.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: pmax_init.c,v 1.6 2001/09/18 16:19:28 ad Exp $	*/
+/*	$NetBSD: pmax_init.c,v 1.7 2001/09/19 20:52:27 thorpej Exp $	*/
 
 /*
  * Copyright (c) 1999, 2000, 2001 The NetBSD Foundation, Inc.
@@ -73,7 +73,7 @@ SOFTWARE.
 #include <sys/proc.h>
 
 #include <machine/tc_machdep.h>
-#include <machine/fbio.h>
+#include <dev/sun/fbio.h>
 #include <machine/fbvar.h>
 #include <machine/pmioctl.h>
 #include <dev/dec/lk201.h>
