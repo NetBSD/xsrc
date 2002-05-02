fe85abd3a21623e44010174202fa3caa2c57c8a1
tron <tron>
2002-05-02 16:40:24 +0000

    Remove files obsoleted by previos XFree86 imports.

--- xfree/xc/programs/Xserver/hw/xfree86/drivers/mga/mga_wrap.h
+++
@@ -1,8 +0,0 @@
-/* $XFree86$ */
-
-#ifndef MGA_WRAP_H
-#define MGA_WRAP_H
-
-void MGADRIWrapFunctions(ScreenPtr pScreen, DRIInfoPtr pDRIInfo);
-
-#endif
--- xfree/xc/util/patch/patchlevel.h
+++
@@ -1,2 +0,0 @@
-/* $Xorg: patchlevel.h,v 1.3 2000/08/17 19:55:22 cpqbld Exp $ */
-#define PATCHLEVEL "12u9.XC2"
