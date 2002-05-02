b1b03a246db34eb34cf7f9414f82df0b870dbc5b
tron <tron@NetBSD.org>
2002-05-02 16:40:24 +0000

    Remove files obsoleted by previos XFree86 imports.

--- xfree/xc/util/patch/version.h
+++
@@ -1,9 +0,0 @@
-/* oldHeader: version.h,v 2.0 86/09/17 15:40:14 lwall Exp $
- * $Xorg: version.h,v 1.3 2000/08/17 19:55:22 cpqbld Exp $
- *
- * Revision 2.0  86/09/17  15:40:14  lwall
- * Baseline for netwide release.
- * 
- */
-
-void version();
