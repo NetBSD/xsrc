87b5f68729358e0c5e7ddc6d584542b5b625a3fc
mycroft <mycroft>
2002-07-17 20:04:09 +0000

    Replace the Alpha-specific PCI probing code with a generic version using /dev/pciN, and use it on macppc.

--- xfree/xc/programs/Xserver/hw/xfree86/os-support/bus/Pci.h
+++ xfree/xc/programs/Xserver/hw/xfree86/os-support/bus/Pci.h
@@ -147,6 +147,9 @@
 #elif defined(__powerpc__) && defined(__OpenBSD__)
 # define  ARCH_PCI_INIT freebsdPciInit
 # define INCLUDE_XF86_MAP_PCI_MEM
+#elif defined(__powerpc__) && defined(__NetBSD__)
+# define ARCH_PCI_INIT netbsdPciInit
+# define INCLUDE_XF86_MAP_PCI_MEM
 #elif defined(__powerpc__)
 # define ARCH_PCI_INIT ppcPciInit
 # if !defined(PowerMAX_OS)
--- xfree/xc/programs/Xserver/hw/xfree86/os-support/bus/netbsdPci.c
+++ xfree/xc/programs/Xserver/hw/xfree86/os-support/bus/netbsdPci.c
@@ -1,72 +1,47 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/os-support/bus/freebsdPci.c,v 1.1 2000/02/12 20:45:42 dawes Exp $ */
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dev/pci/pciio.h>
 
-/*-
- * Copyright (c) 2001 The NetBSD Foundation, Inc.
- * All rights reserved.
- *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Simon Burge.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+#include "xf86.h"
+#include "xf86Priv.h"
+#include "xf86OSpriv.h"
 
 #include "Pci.h"
 
-#if defined(__alpha__)
-#include <machine/pio.h>
-#include <machine/sysarch.h>
+CARD32 netbsdPciConfRead(PCITAG, int);
+void netbsdPciConfWrite(PCITAG, int, CARD32);
+void netbsdPciSetBits(PCITAG, int, CARD32, CARD32);
 
-/*
- * NetBSD/alpha platform specific PCI access functions.
- * Needs NetBSD kernel version 1.5 or later.
- */
-CARD32 netbsdPciCfgRead(PCITAG tag, int off);
-void netbsdPciCfgWrite(PCITAG, int off, CARD32 val);
-void netbsdPciCfgSetBits(PCITAG tag, int off, CARD32 mask, CARD32 bits);
+static int devpci = -1;
 
 pciBusInfo_t netbsdPci0 = {
-	PCI_CFG_MECH_OTHER, 32, FALSE, 0, 0, 0,
-	{ netbsdPciCfgRead, netbsdPciCfgWrite, netbsdPciCfgSetBits,
-	  pciAddrNOOP, pciAddrNOOP},
-	NULL
+/* configMech  */	PCI_CFG_MECH_OTHER,
+/* numDevices  */	32,
+/* secondary   */	FALSE,
+/* primary_bus */	0,
+/* io_base */		0,	/* XXX */
+/* io_size */		0,	/* XXX */
+/* funcs       */	{
+				netbsdPciConfRead,
+				netbsdPciConfWrite,
+				netbsdPciSetBits,
+				pciAddrNOOP,
+				pciAddrNOOP
+			},
+/* pciBusPriv  */	NULL
 };
 
-#define BUS(tag)	(((tag)>>16)&0xff)
-#define DFN(tag)	(((tag)>>8)&0xff)
-#define DEV(dfn)	(((DFN(tag))>>3)&0x1f)
-#define FNC(dfn)	((DFN(tag))&0x7)
-
-void  
+void
 netbsdPciInit()
 {
-	if (alpha_pci_io_enable(1) != 0)
-		fprintf(stderr, "alpha_pci_io_enable failed!\n");
+
+	devpci = open("/dev/pci0", O_RDWR);
+	if (devpci == -1)
+		FatalError("netbsdPciInit: can't open /dev/pci0\n");
 
 	pciNumBuses    = 1;
 	pciBusInfo[0]  = &netbsdPci0;
@@ -75,26 +50,44 @@ netbsdPciInit()
 }
 
 CARD32
-netbsdPciCfgRead(PCITAG tag, int off)
+netbsdPciConfRead(PCITAG tag, int reg)
 {
-	return(alpha_pci_conf_read(BUS(tag), DEV(tag), FNC(tag), off));
-		return ~0;
+	struct pciio_bdf_cfgreg bdfr;
+
+	bdfr.bus      = PCI_BUS_FROM_TAG(tag);
+	bdfr.device   = PCI_DEV_FROM_TAG(tag);
+	bdfr.function = PCI_FUNC_FROM_TAG(tag);
+	bdfr.cfgreg.reg = reg;
+
+	if (ioctl(devpci, PCI_IOC_BDF_CFGREAD, &bdfr) == -1)
+		FatalError("netbsdPciConfRead: failed on %d/%d/%d\n",
+		    bdfr.bus, bdfr.device, bdfr.function);
+
+	return (bdfr.cfgreg.val);
 }
 
 void
-netbsdPciCfgWrite(PCITAG tag, int off, CARD32 val)
+netbsdPciConfWrite(PCITAG tag, int reg, CARD32 val)
 {
-	alpha_pci_conf_write(BUS(tag), DEV(tag), FNC(tag), off, val);
+	struct pciio_bdf_cfgreg bdfr;
+
+	bdfr.bus      = PCI_BUS_FROM_TAG(tag);
+	bdfr.device   = PCI_DEV_FROM_TAG(tag);
+	bdfr.function = PCI_FUNC_FROM_TAG(tag);
+	bdfr.cfgreg.reg = reg;
+	bdfr.cfgreg.val = val;
+
+	if (ioctl(devpci, PCI_IOC_BDF_CFGWRITE, &bdfr) == -1)
+		FatalError("netbsdPciConfWrite: failed on %d/%d/%d\n",
+		    bdfr.bus, bdfr.device, bdfr.function);
 }
 
 void
-netbsdPciCfgSetBits(PCITAG tag, int off, CARD32 mask, CARD32 bits)
+netbsdPciSetBits(PCITAG tag, int reg, CARD32 mask, CARD32 bits)
 {
-	CARD32	val = netbsdPciCfgRead(tag, off);
+	CARD32 val;
+
+	val = netbsdPciConfRead(tag, reg);
 	val = (val & ~mask) | (bits & mask);
-	netbsdPciCfgWrite(tag, off, val);
+	netbsdPciConfWrite(tag, reg, val);
 }
-/* #elif defined(__something_else__) */
-#else
-# error architecture not supported
-#endif
