33faf179e610799bbbeb26790ca865de8b9aba7c
itojun <itojun@NetBSD.org>
2002-09-13 09:41:12 +0000

    pull in 4.2.1 erratta (affects 3.3.6 too)
      * 4.2.1 Errata
          + The MIT-SHM update in 4.2.1 is incomplete. The case where the X server
            is started from xdm was not handled. A more complete fix from XFree86
            trunk has committed to the xf-4_2-branch branch.
            A source patch against 4.2.1 is available on the XFree86 FTP site.

--- xc/programs/Xserver/include/os.h
+++ xc/programs/Xserver/include/os.h
@@ -586,6 +586,8 @@ extern int LocalClient(
 #endif
 );
 
+extern int LocalClientCred(ClientPtr, int *, int *);
+
 extern int ChangeAccessControl(
 #if NeedFunctionPrototypes
     ClientPtr /*client*/,
