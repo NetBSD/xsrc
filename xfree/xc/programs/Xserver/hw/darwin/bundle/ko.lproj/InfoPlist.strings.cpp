d6257521be6abc6651598a7d11662e7ed719aff5
tron <tron@NetBSD.org>
2003-02-28 14:10:54 +0000

    Resolve conflicts from import of XFree86 4.3.0.

--- xfree/xc/programs/Xserver/hw/darwin/bundle/ko.lproj/InfoPlist.strings.cpp
+++
@@ -1,5 +0,0 @@
-/* Korean localized versions of Info.plist keys. */
-/* Most of these are set in the target application settings. */
-/* $XFree86: xc/programs/Xserver/hw/darwin/bundle/ko.lproj/InfoPlist.strings.cpp,v 1.1.4.2 2002/08/20 22:10:52 torrey Exp $ */
-
-NSHumanReadableCopyright = __quote__XFree86 XF86_VERSION\nCopyright 2002 XFree86 Project, Inc.__quote__;
--- xfree/xc/programs/Xserver/hw/xfree86/os-support/xf86drmSiS.h
+++
@@ -1,8 +0,0 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/os-support/xf86drmSiS.h,v 1.1 2001/12/15 00:59:12 dawes Exp $ */
-
-#ifndef __XF86DRI_SIS_H__
-#define __XF86DRI_SIS_H__
-
-extern Bool drmSiSAgpInit(int driSubFD, int offset, int size);
-
-#endif
