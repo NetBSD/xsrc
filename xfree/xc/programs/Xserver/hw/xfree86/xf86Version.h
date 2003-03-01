9079beb1031c9604d30fc2d6044596ae5a9f102d
tron <tron>
2003-03-01 07:54:37 +0000

    Resolve conflicts from import of XFree86 4.3.0.

--- xfree/xc/programs/Xserver/hw/xfree86/xf86Version.h
+++ xfree/xc/programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,15 +1,20 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.487.2.5 2002/09/04 03:01:47 dawes Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.543 2003/02/27 04:56:45 dawes Exp $ */
 
 #ifndef XF86_VERSION_CURRENT
 
 #define XF86_VERSION_MAJOR	4
-#define XF86_VERSION_MINOR	2
-#define XF86_VERSION_PATCH	1
+#define XF86_VERSION_MINOR	3
+#define XF86_VERSION_PATCH	0
 #define XF86_VERSION_SNAP	0
 
 /* This has five arguments for compatibilty reasons */
 #define XF86_VERSION_NUMERIC(major,minor,patch,snap,dummy) \
 	(((major) * 10000000) + ((minor) * 100000) + ((patch) * 1000) + snap)
+
+#define XF86_GET_MAJOR_VERSION(vers)	((vers) / 10000000)
+#define XF86_GET_MINOR_VERSION(vers)	(((vers) % 10000000) / 100000)
+#define XF86_GET_PATCH_VERSION(vers)	(((vers) % 100000) / 1000)
+#define XF86_GET_SNAP_VERSION(vers)	((vers) % 1000)
 
 /* Define these for compatibility.  They'll be removed at some point. */
 #define XF86_VERSION_SUBMINOR	XF86_VERSION_PATCH
@@ -22,9 +27,6 @@
 			XF86_VERSION_PATCH,			\
 			XF86_VERSION_SNAP,			\
 			0)
-
-
-#define XF86_DATE	"3 September 2002"
 
 #endif
 
