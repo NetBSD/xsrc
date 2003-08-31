092bdd0ca0960db0b42ba988cb0f24b8d337f5fa
tron <tron>
2003-08-31 07:55:55 +0000

    On operating systems which don't define "SIZE_T_MAX" (NetBSD does) define it to "ULONG_MAX" if possible. This should avoid problems on non-NetBSD 64Bit platforms (e.g. Solaris). Based on a suggestion made by Christoph Egger in private e-mail.

--- xfree/xc/lib/font/fc/fslibos.h
+++ xfree/xc/lib/font/fc/fslibos.h
@@ -57,7 +57,11 @@ from The Open Group.
 # endif
 #endif
 #ifndef SIZE_T_MAX
-# define SIZE_T_MAX UINT_MAX
+# ifdef ULONG_MAX
+#  define SIZE_T_MAX ULONG_MAX
+# else
+#  define SIZE_T_MAX UINT_MAX
+# endif
 #endif
 #ifndef OPEN_MAX
 #if defined(SVR4) || defined(__UNIXOS2__)
