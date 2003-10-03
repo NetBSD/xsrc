488cdde06ce44fd2c9e95a78d4db66cc75178a8d
itojun <itojun>
2003-10-03 09:05:55 +0000

    better handling of (len % 4) portion

--- xfree/xc/programs/Xserver/os/auth.c
+++ xfree/xc/programs/Xserver/os/auth.c
@@ -404,9 +404,9 @@ GenerateRandomData (int len, char *buf)
     {
 	rnd[i] = arc4random();
     }
-    for (i = 0; i < len % 4; i++)
+    for (i = (len / 4) * 4; i < len; i++)
     {
-	buf[(len / 4) * 4 + i] = arc4random() & 0xff;
+	buf[i] = arc4random() & 0xff;
     }
 #else
     static int seed;
