0738add7835d20d5155746b08b9ca1da2f7f7433
chris <chris>
2004-03-28 15:24:25 +0000

    Fix the loader to work on arm.  This is enough for the nv driver to load on my cats box.
    There maybe other softfloat symbols needed by other modules, but they
    should be easy to add.

--- xfree/xc/programs/Xserver/hw/xfree86/loader/loader.c
+++ xfree/xc/programs/Xserver/hw/xfree86/loader/loader.c
@@ -579,8 +579,12 @@ _LoaderFileToMem(int fd, unsigned long offset, int size, char *label)
 	ppc_flush_icache(ptr + size - 1);
     }
 # endif
-#if defined(__NetBSD__) && defined(__powerpc__)
+#if defined(__NetBSD__)
+#if defined(__powerpc__)
     __syncicache(ptr,size);
+#elif defined(__arm__)
+    arm_sync_icache(ptr, size);
+#endif
 #endif
 
 # ifdef DEBUGMEM
