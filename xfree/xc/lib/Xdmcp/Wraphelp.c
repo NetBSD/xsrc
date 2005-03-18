892f4eb7b611966a12851a9816cfb5d32d987776
tron <tron>
2005-03-18 15:51:29 +0000

    Fix declaration of _XdmcpAuthSetup() and _XdmcpAuthDoIt() to make this build again.

--- xfree/xc/lib/Xdmcp/Wraphelp.c
+++ xfree/xc/lib/Xdmcp/Wraphelp.c
@@ -1,18 +1,9 @@
-/* $XFree86: xc/lib/Xdmcp/Wraphelp.c,v 1.1 2004/06/09 18:30:56 dawes Exp $ */
+/* $NetBSD: Wraphelp.c,v 1.2 2005/03/18 15:51:29 tron Exp $ */
 
-/*
- * Due to U.S. Exports Regulations, this code is subject to the following
- * notice:
- *
- *   This is publicly available encryption source code which, together with
- *   object code resulting from the compiling of publicly available source
- *   code, may be exported from the United States under License Exception
- *   "TSU" of the US Export Administration Regulations (EAR), codified at
- *   15 Code of Federal Regulations, Chapter 7.
- *
- * See <http://www.bxa.doc.gov/Encryption/PubAvailEncSourceCodeNofify.html>
- * for the notification requirements for "Publicly Available" encryption
- * source code.
+/* Export of this software from the United States of America is assumed
+ * to require a specific license from the United States Government.
+ * It is the responsibility of any person or organization contemplating
+ * export to obtain such a license before exporting.
  */
 
 /*
@@ -127,8 +118,9 @@ static	char	e[] = {
  * Set up the key schedule from the key.
  */
 
-static void
-setkey(char *key)
+static
+setkey(key)
+char *key;
 {
 	int	i, j, k;
 	int	t;
@@ -191,45 +183,45 @@ setkey(char *key)
  * index, unlike everything else.
  */
 static	char	S[8][64] = {
-	{14, 4,13, 1, 2,15,11, 8, 3,10, 6,12, 5, 9, 0, 7,
-	  0,15, 7, 4,14, 2,13, 1,10, 6,12,11, 9, 5, 3, 8,
-	  4, 1,14, 8,13, 6, 2,11,15,12, 9, 7, 3,10, 5, 0,
-	 15,12, 8, 2, 4, 9, 1, 7, 5,11, 3,14,10, 0, 6,13},
+	14, 4,13, 1, 2,15,11, 8, 3,10, 6,12, 5, 9, 0, 7,
+	 0,15, 7, 4,14, 2,13, 1,10, 6,12,11, 9, 5, 3, 8,
+	 4, 1,14, 8,13, 6, 2,11,15,12, 9, 7, 3,10, 5, 0,
+	15,12, 8, 2, 4, 9, 1, 7, 5,11, 3,14,10, 0, 6,13,
 
-	{15, 1, 8,14, 6,11, 3, 4, 9, 7, 2,13,12, 0, 5,10,
-	  3,13, 4, 7,15, 2, 8,14,12, 0, 1,10, 6, 9,11, 5,
-	  0,14, 7,11,10, 4,13, 1, 5, 8,12, 6, 9, 3, 2,15,
-	 13, 8,10, 1, 3,15, 4, 2,11, 6, 7,12, 0, 5,14, 9},
+	15, 1, 8,14, 6,11, 3, 4, 9, 7, 2,13,12, 0, 5,10,
+	 3,13, 4, 7,15, 2, 8,14,12, 0, 1,10, 6, 9,11, 5,
+	 0,14, 7,11,10, 4,13, 1, 5, 8,12, 6, 9, 3, 2,15,
+	13, 8,10, 1, 3,15, 4, 2,11, 6, 7,12, 0, 5,14, 9,
 
-	{10, 0, 9,14, 6, 3,15, 5, 1,13,12, 7,11, 4, 2, 8,
-	 13, 7, 0, 9, 3, 4, 6,10, 2, 8, 5,14,12,11,15, 1,
-	 13, 6, 4, 9, 8,15, 3, 0,11, 1, 2,12, 5,10,14, 7,
-	  1,10,13, 0, 6, 9, 8, 7, 4,15,14, 3,11, 5, 2,12},
+	10, 0, 9,14, 6, 3,15, 5, 1,13,12, 7,11, 4, 2, 8,
+	13, 7, 0, 9, 3, 4, 6,10, 2, 8, 5,14,12,11,15, 1,
+	13, 6, 4, 9, 8,15, 3, 0,11, 1, 2,12, 5,10,14, 7,
+	 1,10,13, 0, 6, 9, 8, 7, 4,15,14, 3,11, 5, 2,12,
 
-	{ 7,13,14, 3, 0, 6, 9,10, 1, 2, 8, 5,11,12, 4,15,
-	 13, 8,11, 5, 6,15, 0, 3, 4, 7, 2,12, 1,10,14, 9,
-	 10, 6, 9, 0,12,11, 7,13,15, 1, 3,14, 5, 2, 8, 4,
-	  3,15, 0, 6,10, 1,13, 8, 9, 4, 5,11,12, 7, 2,14},
+	 7,13,14, 3, 0, 6, 9,10, 1, 2, 8, 5,11,12, 4,15,
+	13, 8,11, 5, 6,15, 0, 3, 4, 7, 2,12, 1,10,14, 9,
+	10, 6, 9, 0,12,11, 7,13,15, 1, 3,14, 5, 2, 8, 4,
+	 3,15, 0, 6,10, 1,13, 8, 9, 4, 5,11,12, 7, 2,14,
 
-	{ 2,12, 4, 1, 7,10,11, 6, 8, 5, 3,15,13, 0,14, 9,
-	 14,11, 2,12, 4, 7,13, 1, 5, 0,15,10, 3, 9, 8, 6,
-	  4, 2, 1,11,10,13, 7, 8,15, 9,12, 5, 6, 3, 0,14,
-	 11, 8,12, 7, 1,14, 2,13, 6,15, 0, 9,10, 4, 5, 3},
+	 2,12, 4, 1, 7,10,11, 6, 8, 5, 3,15,13, 0,14, 9,
+	14,11, 2,12, 4, 7,13, 1, 5, 0,15,10, 3, 9, 8, 6,
+	 4, 2, 1,11,10,13, 7, 8,15, 9,12, 5, 6, 3, 0,14,
+	11, 8,12, 7, 1,14, 2,13, 6,15, 0, 9,10, 4, 5, 3,
 
-	{12, 1,10,15, 9, 2, 6, 8, 0,13, 3, 4,14, 7, 5,11,
-	 10,15, 4, 2, 7,12, 9, 5, 6, 1,13,14, 0,11, 3, 8,
-	  9,14,15, 5, 2, 8,12, 3, 7, 0, 4,10, 1,13,11, 6,
-	  4, 3, 2,12, 9, 5,15,10,11,14, 1, 7, 6, 0, 8,13},
+	12, 1,10,15, 9, 2, 6, 8, 0,13, 3, 4,14, 7, 5,11,
+	10,15, 4, 2, 7,12, 9, 5, 6, 1,13,14, 0,11, 3, 8,
+	 9,14,15, 5, 2, 8,12, 3, 7, 0, 4,10, 1,13,11, 6,
+	 4, 3, 2,12, 9, 5,15,10,11,14, 1, 7, 6, 0, 8,13,
 
-	{ 4,11, 2,14,15, 0, 8,13, 3,12, 9, 7, 5,10, 6, 1,
-	 13, 0,11, 7, 4, 9, 1,10,14, 3, 5,12, 2,15, 8, 6,
-	  1, 4,11,13,12, 3, 7,14,10,15, 6, 8, 0, 5, 9, 2,
-	  6,11,13, 8, 1, 4,10, 7, 9, 5, 0,15,14, 2, 3,12},
+	 4,11, 2,14,15, 0, 8,13, 3,12, 9, 7, 5,10, 6, 1,
+	13, 0,11, 7, 4, 9, 1,10,14, 3, 5,12, 2,15, 8, 6,
+	 1, 4,11,13,12, 3, 7,14,10,15, 6, 8, 0, 5, 9, 2,
+	 6,11,13, 8, 1, 4,10, 7, 9, 5, 0,15,14, 2, 3,12,
 
-	{13, 2, 8, 4, 6,15,11, 1,10, 9, 3,14, 5, 0,12, 7,
-	  1,15,13, 8,10, 3, 7, 4,12, 5, 6,11, 0,14, 9, 2,
-	  7,11, 4, 1, 9,12,14, 2, 0, 6,10,13,15, 3, 5, 8,
-	  2, 1,14, 7, 4,10, 8,13,15,12, 9, 0, 3, 5, 6,11},
+	13, 2, 8, 4, 6,15,11, 1,10, 9, 3,14, 5, 0,12, 7,
+	 1,15,13, 8,10, 3, 7, 4,12, 5, 6,11, 0,14, 9, 2,
+	 7,11, 4, 1, 9,12,14, 2, 0, 6,10,13,15, 3, 5, 8,
+	 2, 1,14, 7, 4,10, 8,13,15,12, 9, 0, 3, 5, 6,11,
 };
 
 /*
@@ -265,11 +257,12 @@ static	char	preS[48];
  * The payoff: encrypt a block.
  */
 
-static void
-encrypt (char *block, int edflag)
+static
+encrypt (block, edflag)
+char *block;
 {
 	int i, ii;
-	register int t, j, k;
+	register t, j, k;
 
 	/*
 	 * First, permute the bits in the input
@@ -354,8 +347,10 @@ encrypt (char *block, int edflag)
 		block[j] = L[FP[j]-1];
 }
 
-static void
-bytes_to_bits (unsigned char *bytes, char *bits)
+static
+bytes_to_bits (bytes, bits)
+    unsigned char   *bytes;
+    char	    *bits;
 {
     int	    bit, byte, value;
 
@@ -367,8 +362,10 @@ bytes_to_bits (unsigned char *bytes, char *bits)
     }
 }
 
-static void
-bits_to_bytes (char *bits, unsigned char *bytes)
+static
+bits_to_bytes (bits, bytes)
+    char	    *bits;
+    unsigned char   *bytes;
 {
     int	    bit, byte, value;
 
@@ -389,7 +386,9 @@ bits_to_bytes (char *bits, unsigned char *bytes)
 
 /*ARGSUSED*/
 void
-_XdmcpAuthSetup (auth_cblock key, auth_wrapper_schedule schedule)
+_XdmcpAuthSetup (key, schedule)
+    auth_cblock		key;
+    auth_wrapper_schedule	schedule;
 {
     char    expand_key[64];
 
@@ -399,8 +398,10 @@ _XdmcpAuthSetup (auth_cblock key, auth_wrapper_schedule schedule)
 
 /*ARGSUSED*/
 void
-_XdmcpAuthDoIt (auth_cblock input, auth_cblock output,
-		auth_wrapper_schedule schedule, int edflag)
+_XdmcpAuthDoIt (input, output, schedule, edflag)
+    auth_cblock		input, output;
+    auth_wrapper_schedule	schedule;
+    int			edflag;
 {
     char    expand_input[64];
 
