aceba2840a5df92aa560f48ef21d8eb2c7b63ab8
tron <tron@NetBSD.org>
2005-03-18 16:46:54 +0000

    Fix mismerge.

--- xfree/xc/programs/Xserver/hw/xfree86/os-support/xf86OSmouse.h
+++ xfree/xc/programs/Xserver/hw/xfree86/os-support/xf86OSmouse.h
@@ -233,7 +233,6 @@ typedef struct _MouseDevRec {
     const char *	protocol;
     MouseProtocolID	protocolID;
     MouseProtocolID	oldProtocolID; /* hack */
-    int			xisbscale;	/* buffer size for 1 event */
     int			class;
     int			mseModel;
     int			baudRate;
