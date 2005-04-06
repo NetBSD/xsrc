7d1f958db365f9a316598fb09404e5e37caa1396
tron <tron>
2005-04-06 11:20:55 +0000

    Pull up revision 1.5 (requested by jmc in ticket #1033): Adjust these to not use MACHINE and instead pull it from uname(3) instead. This means xbase.tgz can be shared across MACHINE_ARCH correctly now.

--- xfree/xc/programs/xman/misc.c
+++ xfree/xc/programs/xman/misc.c
@@ -44,6 +44,10 @@ from the X Consortium.
 #include <X11/Xaw/Dialog.h>
 #include <X11/Shell.h>
 
+#if defined(__OpenBSD__) || defined(__NetBSD__)
+#include <sys/utsname.h>
+#endif
+
 static FILE * Uncompress(ManpageGlobals * man_globals, char * filename);
 #ifndef HAS_MKSTEMP
 static Boolean UncompressNamed(ManpageGlobals * man_globals, char * filename,
@@ -188,6 +192,9 @@ FindManualFile(ManpageGlobals * man_globals, int section_num, int entry_num)
 #if defined(ISC) || defined(SCO)
   int i;
 #endif
+#if defined(__OpenBSD__) || defined(__NetBSD__)
+  struct utsname uts;
+#endif
 
   temp = CreateManpageName(entry, 0, 0);
   sprintf(man_globals->manpage_title, "The current manual page is: %s.", temp);
@@ -199,11 +206,14 @@ FindManualFile(ManpageGlobals * man_globals, int section_num, int entry_num)
  * Look for uncompressed files first.
  */
 #if defined(__OpenBSD__) || defined(__NetBSD__)
-  /* look in machine subdir first */
-  sprintf(filename, "%s/%s%s/%s/%s", path, CAT,
-	  section + len_cat, MACHINE, page);
-  if ( (file = fopen(filename,"r")) != NULL)
-    return(file);
+  /* XXX: No way to deal w. error here so just move on. */
+  if (uname(&uts) == 0) {
+    /* look in machine subdir first */
+    sprintf(filename, "%s/%s%s/%s/%s", path, CAT,
+  	  section + len_cat, uts.machine, page);
+    if ( (file = fopen(filename,"r")) != NULL)
+      return(file);
+  }
 #endif
 
   sprintf(filename, "%s/%s%s/%s", path, CAT, section + len_cat, page);
@@ -216,11 +226,14 @@ FindManualFile(ManpageGlobals * man_globals, int section_num, int entry_num)
 
 #if !defined(ISC) && !defined(SCO)
 #if defined(__OpenBSD__) || defined(__NetBSD__)
-  /* look in machine subdir first */
-  sprintf(filename, "%s/%s%s/%s/%s.%s", path, CAT,
-	  section + len_cat, MACHINE, page, COMPRESSION_EXTENSION);
-  if ( (file = Uncompress(man_globals, filename)) != NULL)
-    return(file);
+  /* XXX: No way to deal w. error here so just move on. */
+  if (uname(&uts) == 0) {
+    /* look in machine subdir first */
+    sprintf(filename, "%s/%s%s/%s/%s.%s", path, CAT,
+	  section + len_cat, uts.machine, page, COMPRESSION_EXTENSION);
+    if ( (file = Uncompress(man_globals, filename)) != NULL)
+      return(file);
+  }
 #endif
   sprintf(filename, "%s/%s%s/%s.%s", path, CAT,
 	  section + len_cat, page, COMPRESSION_EXTENSION);
@@ -229,11 +242,14 @@ FindManualFile(ManpageGlobals * man_globals, int section_num, int entry_num)
 #ifdef GZIP_EXTENSION
   else {
 #if defined(__OpenBSD__) || defined(__NetBSD__)
-      /* look in machine subdir first */
-      sprintf(filename, "%s/%s%s/%s/%s.%s", path, CAT,
-	      section + len_cat, MACHINE, page, GZIP_EXTENSION);
-      if ( (file = Uncompress(man_globals, filename)) != NULL)
-	  return(file);
+      /* XXX: No way to deal w. error here so just move on. */
+      if (uname(&uts) == 0) {
+        /* look in machine subdir first */
+        sprintf(filename, "%s/%s%s/%s/%s.%s", path, CAT,
+	      section + len_cat, uts.machine, page, GZIP_EXTENSION);
+        if ( (file = Uncompress(man_globals, filename)) != NULL)
+  	  return(file);
+      }
 #endif
     sprintf(filename, "%s/%s%s/%s.%s", path, CAT,
 	    section + len_cat, page, GZIP_EXTENSION);
@@ -725,23 +741,29 @@ UncompressUnformatted(ManpageGlobals * man_globals, char * entry,
 		      char * filename, FILE **file)
 #endif
 {
+#if defined(__OpenBSD__) || defined(__NetBSD__)
+  struct utsname uts;
+#endif
   char path[BUFSIZ], page[BUFSIZ], section[BUFSIZ], input[BUFSIZ];
   int len_cat = strlen(CAT), len_man = strlen(MAN);
 
   ParseEntry(entry, path, section, page);
 
 #if defined(__OpenBSD__) || defined(__NetBSD__)
-  /*
-   * look for uncomressed file in machine subdir first
-   */
-  sprintf(filename, "%s/%s%s/%s/%s", path, MAN,
-	  section + len_cat, MACHINE, page);
-  if ( access( filename, R_OK ) == 0 ) {
-    man_globals->compress = FALSE;
-    man_globals->gzip = FALSE;
-    sprintf(man_globals->save_file, "%s/%s%s/%s/%s", path,
-	    CAT, section + len_cat, MACHINE, page);
-    return(TRUE);
+  /* XXX: No way to deal w. error here so just move on. */
+  if (uname(&uts) == 0) {
+    /*
+     * look for uncomressed file in machine subdir first
+     */
+    sprintf(filename, "%s/%s%s/%s/%s", path, MAN,
+	  section + len_cat, uts.machine, page);
+    if ( access( filename, R_OK ) == 0 ) {
+      man_globals->compress = FALSE;
+      man_globals->gzip = FALSE;
+      sprintf(man_globals->save_file, "%s/%s%s/%s/%s", path,
+	    CAT, section + len_cat, uts.machine, page);
+      return(TRUE);
+    }
   }
  /*
   * Then for compressed files in an uncompressed directory.
@@ -959,6 +981,9 @@ void
 ParseEntry(char *entry, char *path, char *sect, char *page)
 {
   char *c, temp[BUFSIZ];
+#if defined(__OpenBSD__) || defined(__NetBSD__)
+  struct utsname uts;
+#endif
 
   strcpy(temp, entry);
 
@@ -974,12 +999,15 @@ ParseEntry(char *entry, char *path, char *sect, char *page)
     PrintError("index failure in ParseEntry.");
   *c++ = '\0';
 #if defined(__OpenBSD__) || defined(__NetBSD__)
-  /* Skip machine subdirectory if present */
-  if (strcmp(c, MACHINE) == 0) {
-      c = rindex(temp, '/');
-      if (c == NULL)
-	  PrintError("index failure in ParseEntry.");
-      *c++ = '\0';
+  /* XXX: No way to deal w. error here so just move on. */
+  if (uname(&uts) == 0) {
+    /* Skip machine subdirectory if present */
+    if (strcmp(c, uts.machine) == 0) {
+        c = rindex(temp, '/');
+        if (c == NULL)
+	    PrintError("index failure in ParseEntry.");
+        *c++ = '\0';
+    }
   }
 #endif
   if (sect != NULL)
