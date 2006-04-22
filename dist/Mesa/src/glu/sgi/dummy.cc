57bbbeca206eb636edafc570080d5d356e9ae6bb
macallan <macallan@NetBSD.org>
2006-04-22 15:17:13 +0000

    import Mesa 6.4.1, needed by xorg

---
+++ dist/Mesa/src/glu/sgi/dummy.cc
@@ -0,0 +1,5 @@
+/* $Id: dummy.cc,v 1.1.1.1 2006/04/22 15:22:44 macallan Exp $ */
+/*
+ * This file contains nothing. It's just there so there's at least a single
+ * source file for libGLU.la in this directory.
+ */
