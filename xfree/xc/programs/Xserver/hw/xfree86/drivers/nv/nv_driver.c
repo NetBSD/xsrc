d830f61f152e482a7c0c17b24c2055dda8abdcc0
jmmv <jmmv>
2006-07-30 18:17:34 +0000

    xf86LoadVBEModule does not exist so use xf86LoadSubModule instead.  This is to avoid an unresolved symbol in the resulting drivers, a problem that was introduced when sync'ing them with X.Org sources back in Dec 2005.
    Problem reported by Greg A. Woods.  Change approved by tron@.

--- xfree/xc/programs/Xserver/hw/xfree86/drivers/nv/nv_driver.c
+++ xfree/xc/programs/Xserver/hw/xfree86/drivers/nv/nv_driver.c
@@ -936,7 +936,7 @@ nvProbeDDC(ScrnInfoPtr pScrn, int index)
 {
     vbeInfoPtr pVbe;
 
-    if (xf86LoadVBEModule(pScrn)) {
+    if (xf86LoadSubModule(pScrn, "vbe")) {
         pVbe = VBEInit(NULL,index);
         ConfiguredMonitor = vbeDoEDID(pVbe, NULL);
 	vbeFree(pVbe);
