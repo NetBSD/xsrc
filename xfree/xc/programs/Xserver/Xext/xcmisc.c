b60677312551feedcae705345cb81ce4d6077774
bouyer <bouyer>
2007-04-05 21:04:19 +0000

    Pull up following revision(s) (requested by drochner in ticket #11285): 	xfree/xc/extras/freetype2/src/bdf/bdflib.c: revision 1.3 	xfree/xc/lib/X11/ImUtil.c: revision 1.2 	xfree/xc/lib/font/fontfile/fontdir.c: revision 1.2 	xfree/xc/programs/Xserver/Xext/xcmisc.c: revision 1.2 	xfree/xc/lib/font/bitmap/bdfread.c: revision 1.2 fix a possible memory corruption due to integer overflow in ProcXCMiscGetXIDList() (CVE-2007-1003) fix a possible memory corruption due to integer overflow, caused by lack of validation of bdf font files (CVE 2007-1351) fix a possible memory corruption due to integer overflow, caused by lack of validation of fonts.dir files (CVE 2007-1352) fix a possible memory corruption due to incomplete input validation in XInitImage() (CVE 2007-1667) pull in a patch from freetype CVS (CVE-2007-1351): * src/bdf/bdflib.c (setsbit, sbitset): Handle values >= 128   gracefully.   (_bdf_set_default_spacing): Increase `name' buffer size to 256 and   issue an error for longer names.   (_bdf_parse_glyphs): Limit allowed number of glyphs in font to the   number of code points in Unicode.

--- xfree/xc/programs/Xserver/Xext/xcmisc.c
+++ xfree/xc/programs/Xserver/Xext/xcmisc.c
@@ -40,6 +40,12 @@ from The Open Group.
 #include "xcmiscstr.h"
 #include "modinit.h"
 
+#if HAVE_STDINT_H
+#include <stdint.h>
+#elif !defined(UINT32_MAX)
+#define UINT32_MAX 0xffffffffU
+#endif
+
 #if 0
 static unsigned char XCMiscCode;
 #endif
@@ -141,7 +147,10 @@ ProcXCMiscGetXIDList(client)
 
     REQUEST_SIZE_MATCH(xXCMiscGetXIDListReq);
 
-    pids = (XID *)ALLOCATE_LOCAL(stuff->count * sizeof(XID));
+    if (stuff->count > UINT32_MAX / sizeof(XID))
+	return BadAlloc;
+
+    pids = (XID *)Xalloc(stuff->count * sizeof(XID));
     if (!pids)
     {
 	return BadAlloc;
@@ -162,7 +171,7 @@ ProcXCMiscGetXIDList(client)
     	client->pSwapReplyFunc = (ReplySwapPtr) Swap32Write;
 	WriteSwappedDataToClient(client, count * sizeof(XID), pids);
     }
-    DEALLOCATE_LOCAL(pids);
+    Xfree(pids);
     return(client->noClientException);
 }
 
