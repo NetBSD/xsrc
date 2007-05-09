4a2c792f8d1af52098538d27b2db0f0f61d4ab2c
ghen <ghen@NetBSD.org>
2007-05-09 10:33:26 +0000

    Pull up following revision(s) (requested by pavel in ticket #629): 	xfree/xc/programs/Xserver/hw/netbsd/arm32vidc/private.h: revision 1.6-1.7 fix the check for versions < 4.99.1 to not include 4.0_BETA2, whose __NetBSD_Version__ is also numerically lower than 499000100. Hopefully this will fix the build faulures on tne netbsd-4 branch. Pointed out by Reinoud Zandijk. make the version test clearer, per suggestion from ghen.

--- xfree/xc/programs/Xserver/hw/netbsd/arm32vidc/private.h
+++ xfree/xc/programs/Xserver/hw/netbsd/arm32vidc/private.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: private.h,v 1.5 2006/08/14 22:12:59 bjh21 Exp $	*/
+/*	$NetBSD: private.h,v 1.5.2.1 2007/05/09 10:33:26 ghen Exp $	*/
 
 /*
  * Copyright (c) 1999 Mark Brinicombe & Neil A. Carson 
@@ -34,7 +34,7 @@
  * still exist.
  */
 #include <sys/param.h>
-#if __NetBSD_Version__ < 499000100
+#if (__NetBSD_Version < 499000100 && __NetBSD_Version >= 499000000) || __NetBSD_Version__ < 400000001
 /* <machine/kbd.h> and <machine/vconsole.h> removed in 4.99.1 */
 #define HAVE_KBD
 #define HAVE_VCONSOLE
