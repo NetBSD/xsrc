dd8b46f2a1f2c92ea0efad8a528435909ce09c06
macallan <macallan@NetBSD.org>
2008-04-19 20:48:12 +0000

    now that we can access IO on shark enable the ISA VGA check here as well. With this X -configure works properly on rev. 4 sharks. Now we have working X on sharks with ct65550s.

--- xfree/xc/programs/Xserver/hw/xfree86/common/xf86Bus.c
+++ xfree/xc/programs/Xserver/hw/xfree86/common/xf86Bus.c
@@ -3000,7 +3000,7 @@ static void
 CheckGenericGA()
 {
 /* This needs to be changed for multiple domains */
-#if !defined(__arm__) && !defined(__sparc__) && !defined(__powerpc__) && !defined(__mips__) /* FIXME ?? */
+#if /*!defined(__arm__) &&*/ !defined(__sparc__) && !defined(__powerpc__) && !defined(__mips__) /* FIXME ?? */
     IOADDRESS GenericIOBase = VGAHW_GET_IOBASE();
     CARD8 CurrentValue, TestValue;
 
