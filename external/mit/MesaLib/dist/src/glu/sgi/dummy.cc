63a9c909c1c2f60c52f6f8d959f93be3226dcea3
mrg <mrg>
2008-07-29 05:08:15 +0000

    initial import of MesaLib-7.0.3

---
+++ external/mit/MesaLib/dist/src/glu/sgi/dummy.cc
@@ -0,0 +1,5 @@
+/* $Id: dummy.cc,v 1.1.1.1 2008/07/29 05:10:33 mrg Exp $ */
+/*
+ * This file contains nothing. It's just there so there's at least a single
+ * source file for libGLU.la in this directory.
+ */
