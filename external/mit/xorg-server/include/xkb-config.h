50d8ec9f2cde168fc922c0a1443bde5e8681d1e2
rtr <rtr@NetBSD.org>
2008-08-03 10:41:55 +0000

    comment out definitions for XKB_BASE_DIRECTORY XKB_BIN_DIRECTORY and XKM_OUTPUT_DIR.  the values defined were not correct anyway instead we rely on CPPFLAGS from xkb Makefile which are share/mk aware

--- external/mit/xorg-server/include/xkb-config.h
+++ external/mit/xorg-server/include/xkb-config.h
@@ -9,14 +9,19 @@
 /* Default set of XKB rules. */
 #define __XKBDEFRULES__ "xorg"
 
+/*
+ * the following definitions are provided by a combination of
+ * bsd.x11.mk and external/mit/xorg/server/xorg-server/xkb/Makefile
+ */
+
 /* Path to XKB definitions. */
-#define XKB_BASE_DIRECTORY "/usr/local/share/X11/xkb"
+/* #define XKB_BASE_DIRECTORY "/usr/local/share/X11/xkb" */
 
 /* Path to xkbcomp. */
-#define XKB_BIN_DIRECTORY "/usr/local/bin"
+/* #define XKB_BIN_DIRECTORY "/usr/local/bin" */
 
 /* XKB output dir for compiled keymaps. */
-#define XKM_OUTPUT_DIR "/usr/local/share/X11/xkb/compiled/"
+/* #define XKM_OUTPUT_DIR "/usr/local/share/X11/xkb/compiled/" */
 
 /* Do not have `strcasecmp'. */
 /* #undef NEED_STRCASECMP */
