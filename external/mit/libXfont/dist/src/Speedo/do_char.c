c7449593f6c5e35766160f49d03edc1656df727d
christos <christos@NetBSD.org>
2008-08-31 12:10:47 +0000

    fix debugging build

--- external/mit/libXfont/dist/src/Speedo/do_char.c
+++ external/mit/libXfont/dist/src/Speedo/do_char.c
@@ -35,8 +35,6 @@ WITH THE SPEEDO SOFTWARE OR THE BITSTREAM CHARTER OUTLINE FONT.
 #endif
 #include "spdo_prv.h"               /* General definitions for Speedo    */
 
-#define   DEBUG   0
-
 #if DEBUG
 #include <stdio.h>
 #define SHOW(X) printf("X = %d\n", X)
