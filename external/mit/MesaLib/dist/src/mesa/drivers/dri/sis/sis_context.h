7cf1cee04567d0674279957c732cfb314536f3df
macallan <macallan>
2008-10-22 16:06:44 +0000

    use membar_sync() from atomic.h instead of loads of arch-specific __asm Suggested by bjs

--- external/mit/MesaLib/dist/src/mesa/drivers/dri/sis/sis_context.h
+++ external/mit/MesaLib/dist/src/mesa/drivers/dri/sis/sis_context.h
@@ -401,11 +401,16 @@ struct sis_context
 #define MMIO_READ(reg) *(volatile GLint *)(smesa->IOBase + (reg))
 #define MMIO_READf(reg) *(volatile GLfloat *)(smesa->IOBase + (reg))
 
+#if defined(__NetBSD__)
+#include <sys/atomic.h>
+#define MMIO_WMB() membar_sync()
+#else /* !__NetBSD__ */
 #if defined(__i386__) || defined(__amd64__)
 #define MMIO_WMB()	__asm __volatile("" : : : "memory")
 #else
 #error platform needs WMB
 #endif
+#endif /* __NetBSD__ */
 
 #define mEndPrimitive()  \
 {       \
