d4128e93441576451ac300882d039dfbba4435e1
snj <snj@NetBSD.org>
2009-03-15 20:30:15 +0000

    Pull up following revision(s) (requested by plunky in ticket #574): 	external/mit/xdm/dist/config/Xreset.cpp: revision 1.2 	external/mit/xdm/dist/config/Xstartup.cpp: revision 1.2 cpp does not find tokens inside quotes, so remove the quotes so that the search/replace can happen properly. this was already fixed in upstream sources (Hi David!), reference: http://cgit.freedesktop.org/xorg/app/xdm/commit/?id=b73d0c91d32bf0aa60aa5ce2bbbd4468ce21a445

--- external/mit/xdm/dist/config/Xstartup.cpp
+++ external/mit/xdm/dist/config/Xstartup.cpp
@@ -2,4 +2,4 @@ XCOMM!/bin/sh
 XCOMM Register a login (derived from GiveConsole as follows:)
 XCOMM
 BINDIR/sessreg  -a -w "/var/log/wtmp" -u "/var/run/utmp" \
-	-x "XDMCONFIGDIR/Xservers" -l $DISPLAY -h "" $USER
+	-x XDMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
