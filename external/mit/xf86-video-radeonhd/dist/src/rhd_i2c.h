011619bc8e359890487bf5bf0924668a2ce1dfdc
veego <veego>
2009-04-13 19:33:13 +0000

    Update to 1.2.5
    Note: XVideo support will still not work without other DRI changes.
    
    - Version 1.2.5
     - Added 2D acceleration for R6xx and R7xx.
     - Added XVideo support for R6xx and R7xx.
     - Added support for RS880 and RV790.
     - Added RandR 1.3 mandatory properties.
     - Refactoring of MC code.
     - Enable DRI support by default on R5xx and RS6xx.
     - LUT (color lookup table) fixes.
     - Tons of quirk table entries and bug fixes.
     - Fix register accesses for processors that reorder memory writes.
    
    - Version 1.2.4
     - Added HDMI support.
     - Added support for RV710, RV730 (DCE 3.2).
     - Added screen rotation support.
     - Added RandR 1.3 panning support.
     - Many acceleration and build fixes.

--- external/mit/xf86-video-radeonhd/dist/src/rhd_i2c.h
+++ external/mit/xf86-video-radeonhd/dist/src/rhd_i2c.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007  Luc Verhaegen <lverhaegen@novell.com>
+ * Copyright 2007  Luc Verhaegen <libv@exsuse.de>
  * Copyright 2007  Matthias Hopf <mhopf@novell.com>
  * Copyright 2007  Egbert Eich   <eich@novell.com>
  * Copyright 2007  Advanced Micro Devices, Inc.
@@ -27,8 +27,6 @@
 
 #include "xf86DDC.h"
 #include "rhd.h"
-
-#define I2C_LINES 5
 
 typedef enum {
     RHD_I2C_INIT,
