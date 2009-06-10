6523b1783026af10ef9906dfe325018004f3611a
mrg <mrg@NetBSD.org>
2009-06-10 05:05:52 +0000

    add a config.h for xf86-video-vmware

---
+++ external/mit/xf86-video-vmware/include/config.h
@@ -0,0 +1,76 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+#include "xorg-server.h"
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Has version 1.0.99.901 or greater of the Xserver */
+#define HAVE_XORG_SERVER_1_0_99_901 1
+
+/* Has version 1.1.0 or greater of the Xserver */
+#define HAVE_XORG_SERVER_1_1_0 1
+
+/* Has version 1.2.0 or greater of the Xserver */
+#define HAVE_XORG_SERVER_1_2_0 1
+
+/* Name of package */
+#define PACKAGE "xf86-video-vmware"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=xorg"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "xf86-video-vmware"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "xf86-video-vmware 10.16.6"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xf86-video-vmware"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "10.16.6"
+
+/* Major version of this package */
+#define PACKAGE_VERSION_MAJOR 10
+
+/* Minor version of this package */
+#define PACKAGE_VERSION_MINOR 16
+
+/* Patch version of this package */
+#define PACKAGE_VERSION_PATCHLEVEL 6
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "10.16.6"
