c0e7211b2442d87ba100272456fde3ebebb59629
mrg <mrg@NetBSD.org>
2009-06-10 07:33:39 +0000

    initial import of libXfont-1.4.0

--- external/mit/libXfont/dist/src/fc/fstrans.c
+++ external/mit/libXfont/dist/src/fc/fstrans.c
@@ -1,6 +1,4 @@
 /*
- * $Id: fstrans.c,v 1.1.1.1 2008/07/30 02:48:43 mrg Exp $
- *
  * Copyright © 2004 Keith Packard
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
