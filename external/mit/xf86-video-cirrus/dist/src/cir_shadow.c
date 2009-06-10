8f74f4be052d44b03a6d2c9adfc068a7be5ce6a5
mrg <mrg@NetBSD.org>
2009-06-10 00:46:43 +0000

    initial import of xf86-video-cirrus-1.3.0

--- external/mit/xf86-video-cirrus/dist/src/cir_shadow.c
+++ external/mit/xf86-video-cirrus/dist/src/cir_shadow.c
@@ -19,7 +19,7 @@
 #include "cir.h"
 #include "alp.h"
 
-void
+_X_EXPORT void
 cirRefreshArea(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 {
     CirPtr pCir = CIRPTR(pScrn);
@@ -46,7 +46,7 @@ cirRefreshArea(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
     }
 } 
 
-void
+_X_EXPORT void
 cirPointerMoved(int index, int x, int y)
 {
     ScrnInfoPtr pScrn = xf86Screens[index];
@@ -64,7 +64,7 @@ cirPointerMoved(int index, int x, int y)
     (*pCir->PointerMoved)(index, newX, newY);
 }
 
-void
+_X_EXPORT void
 cirRefreshArea8(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 {
     CirPtr pCir = CIRPTR(pScrn);
@@ -110,7 +110,7 @@ cirRefreshArea8(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 } 
 
 
-void
+_X_EXPORT void
 cirRefreshArea16(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 {
     CirPtr pCir = CIRPTR(pScrn);
@@ -157,7 +157,7 @@ cirRefreshArea16(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 
 
 /* this one could be faster */
-void
+_X_EXPORT void
 cirRefreshArea24(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 {
     CirPtr pCir = CIRPTR(pScrn);
@@ -208,7 +208,7 @@ cirRefreshArea24(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
     }
 }
 
-void
+_X_EXPORT void
 cirRefreshArea32(ScrnInfoPtr pScrn, int num, BoxPtr pbox)
 {
     CirPtr pCir = CIRPTR(pScrn);
