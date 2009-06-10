1524104f0db88568093036340e229efbee52bbca
mrg <mrg>
2009-06-10 02:23:14 +0000

    initial import of xf86-video-trident-1.3.1

--- external/mit/xf86-video-trident/dist/src/trident_dac.c
+++ external/mit/xf86-video-trident/dist/src/trident_dac.c
@@ -21,7 +21,6 @@
  *
  * Author:  Alan Hourihane, alanh@fairlite.demon.co.uk
  */
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/trident/trident_dac.c,v 1.79 2003/11/03 05:11:42 tsi Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -29,7 +28,6 @@
 
 #include "xf86.h"
 #include "xf86_OSproc.h"
-#include "xf86Version.h"
 #include "xf86PciInfo.h"
 #include "xf86Pci.h"
 
