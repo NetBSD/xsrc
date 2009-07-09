496e56261e8c17f30b8c42943078f9e23a0c3a27
mrg <mrg>
2009-07-09 07:30:54 +0000

    update these for xorg-server 1.6.2.

--- external/mit/xorg-server/include/xorg-config.h
+++ external/mit/xorg-server/include/xorg-config.h
@@ -17,7 +17,7 @@
 #define XORGSERVER 1
 
 /* Current X.Org version. */
-#define XORG_VERSION_CURRENT (((1) * 10000000) + ((6) * 100000) + ((1) * 1000) + 901)
+#define XORG_VERSION_CURRENT (((1) * 10000000) + ((6) * 100000) + ((2) * 1000) + 0)
 
 /* Name of X server. */
 #define __XSERVERNAME__ "Xorg"
@@ -85,8 +85,8 @@
 /* BSD /dev/io */
 /* #undef USE_DEV_IO */
 
-/* BSD i386 iopl */
 #if defined(__i386__) || defined(__x86_64__)
+/* BSD i386 iopl */
 #define USE_I386_IOPL 1
 #endif
 
