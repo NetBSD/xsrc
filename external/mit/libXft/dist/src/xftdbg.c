0d590c07f9ec2afb343e84fc079473109f7e7007
mrg <mrg@NetBSD.org>
2009-11-08 09:43:12 +0000

    initial import of libXft-2.1.14

--- external/mit/libXft/dist/src/xftdbg.c
+++ external/mit/libXft/dist/src/xftdbg.c
@@ -1,6 +1,4 @@
 /*
- * $Id: xftdbg.c,v 1.1.1.1 2008/07/30 02:49:10 mrg Exp $
- *
  * Copyright © 2000 Keith Packard
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
