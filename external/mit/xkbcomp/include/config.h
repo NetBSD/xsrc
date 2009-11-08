feed46c6a10f7bd7c4c96cdf875e79bd87161ed3
mrg <mrg@NetBSD.org>
2009-11-08 11:50:27 +0000

    update these for xkbcomp 1.1.1, xwininfo 1.0.5 and xmodmap 1.0.4

--- external/mit/xkbcomp/include/config.h
+++ external/mit/xkbcomp/include/config.h
@@ -44,13 +44,13 @@
 #define PACKAGE_NAME "xkbcomp"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xkbcomp 1.1.0"
+#define PACKAGE_STRING "xkbcomp 1.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xkbcomp"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1.0"
+#define PACKAGE_VERSION "1.1.1"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
@@ -59,10 +59,10 @@
 #define PACKAGE_VERSION_MINOR 1
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 0
+#define PACKAGE_VERSION_PATCHLEVEL 1
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.1.0"
+#define VERSION "1.1.1"
