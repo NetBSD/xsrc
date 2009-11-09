66f7d985457307a7552162d80d1e7a050dc12a58
mrg <mrg@NetBSD.org>
2009-11-09 01:42:09 +0000

    merge  xf86-video-tseng 1.2.3

--- external/mit/xf86-video-tseng/include/config.h
+++ external/mit/xf86-video-tseng/include/config.h
@@ -53,13 +53,13 @@
 #define PACKAGE_NAME "xf86-video-tseng"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xf86-video-tseng 1.2.2"
+#define PACKAGE_STRING "xf86-video-tseng 1.2.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xf86-video-tseng"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.2"
+#define PACKAGE_VERSION "1.2.3"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
@@ -68,10 +68,10 @@
 #define PACKAGE_VERSION_MINOR 2
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 2
+#define PACKAGE_VERSION_PATCHLEVEL 3
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.2.2"
+#define VERSION "1.2.3"
