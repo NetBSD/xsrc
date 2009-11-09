a1f44b2a95f3b2f4ecb88a029f3bf27f3e95e1f0
mrg <mrg@NetBSD.org>
2009-11-09 00:23:49 +0000

    merge xprop 1.1.0

--- external/mit/xprop/include/config.h
+++ external/mit/xprop/include/config.h
@@ -44,25 +44,25 @@
 #define PACKAGE_NAME "xprop"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xprop 1.0.3"
+#define PACKAGE_STRING "xprop 1.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xprop"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.3"
+#define PACKAGE_VERSION "1.1.0"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
 
 /* Minor version of this package */
-#define PACKAGE_VERSION_MINOR 0
+#define PACKAGE_VERSION_MINOR 1
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 3
+#define PACKAGE_VERSION_PATCHLEVEL 0
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.3"
+#define VERSION "1.1.0"
