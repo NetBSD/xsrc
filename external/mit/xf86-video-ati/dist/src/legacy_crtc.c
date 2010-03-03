5e481d12170d1d75b2e4a5a6ca7e1cd371145827
macallan <macallan>
2010-03-03 03:06:50 +0000

    treat the onboard radeon found in Mac Minis like the ones found in iBooks From Hiroaki Urata via PR 42931

--- external/mit/xf86-video-ati/dist/src/legacy_crtc.c
+++ external/mit/xf86-video-ati/dist/src/legacy_crtc.c
@@ -305,7 +305,8 @@ RADEONRestorePLLRegisters(ScrnInfoPtr pScrn,
 
 #if defined(__powerpc__)
     /* apparently restoring the pll causes a hang??? */
-    if (info->MacModel == RADEON_MAC_IBOOK)
+    if ((info->MacModel == RADEON_MAC_IBOOK) ||
+        (info->MacModel == RADEON_MAC_MINI_INTERNAL))
 	return;
 #endif
 
@@ -1242,7 +1243,8 @@ RADEONInitPLLRegisters(ScrnInfoPtr pScrn, RADEONSavePtr save,
 
 #if defined(__powerpc__)
     /* apparently programming this otherwise causes a hang??? */
-    if (info->MacModel == RADEON_MAC_IBOOK)
+    if ((info->MacModel == RADEON_MAC_IBOOK) ||
+        (info->MacModel == RADEON_MAC_MINI_INTERNAL))
 	save->ppll_div_3 = 0x000600ad;
     else
 #endif
