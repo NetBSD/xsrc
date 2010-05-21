eeb80bce7726af22a77d408cc02dbde27a01e121
mrg <mrg@NetBSD.org>
2010-05-21 04:36:34 +0000

    initial import of xdm-1.1.10

--- external/mit/xdm/dist/config/Xstartup.cpp
+++ external/mit/xdm/dist/config/Xstartup.cpp
@@ -1,5 +1,5 @@
 XCOMM!/bin/sh
 XCOMM Register a login (derived from GiveConsole as follows:)
 XCOMM
-BINDIR/sessreg  -a -w WTMP_FILE -u UTMP_FILE \
+exec BINDIR/sessreg  -a -w WTMP_FILE -u UTMP_FILE \
 	-x XDMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
