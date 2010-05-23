02d403d1466905b772d6a0b5e987e7237f36edcd
mrg <mrg>
2010-05-23 21:28:04 +0000

    merge MesaLib 7.7.1

--- external/mit/MesaLib/dist/src/mesa/glapi/glthread.c
+++ external/mit/MesaLib/dist/src/mesa/glapi/glthread.c
@@ -25,16 +25,14 @@
 
 /*
  * XXX There's probably some work to do in order to make this file
- * truly reusable outside of Mesa.
+ * truly reusable outside of Mesa.  First, the glheader.h include must go.
  */
-
 
 #ifdef HAVE_DIX_CONFIG_H
 #include <dix-config.h>
 #endif
 
-#include <stdlib.h>
-#include <stdio.h>
+#include "main/compiler.h"
 #include "glthread.h"
 #include "main/glheader.h"
 
@@ -247,57 +245,6 @@ _glthread_SetTSD(_glthread_TSD *tsd, void *ptr)
 }
 
 #endif /* WIN32_THREADS */
-
-
-
-/*
- * XFree86 has its own thread wrapper, Xthreads.h
- * We wrap it again for GL.
- */
-#ifdef USE_XTHREADS
-
-unsigned long
-_glthread_GetID(void)
-{
-   return (unsigned long) xthread_self();
-}
-
-
-void
-_glthread_InitTSD(_glthread_TSD *tsd)
-{
-   if (xthread_key_create(&tsd->key, NULL) != 0) {
-      perror(INIT_TSD_ERROR);
-      exit(-1);
-   }
-   tsd->initMagic = INIT_MAGIC;
-}
-
-
-void *
-_glthread_GetTSD(_glthread_TSD *tsd)
-{
-   void *ptr;
-   if (tsd->initMagic != INIT_MAGIC) {
-      _glthread_InitTSD(tsd);
-   }
-   xthread_get_specific(tsd->key, &ptr);
-   return ptr;
-}
-
-
-void
-_glthread_SetTSD(_glthread_TSD *tsd, void *ptr)
-{
-   if (tsd->initMagic != INIT_MAGIC) {
-      _glthread_InitTSD(tsd);
-   }
-   xthread_set_specific(tsd->key, ptr);
-}
-
-#endif /* XTHREAD */
-
-
 
 /*
  * BeOS threads
