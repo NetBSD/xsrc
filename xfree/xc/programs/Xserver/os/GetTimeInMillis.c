94695654e5f29e3f9347de2495887252bb0307fc
mrg <mrg@NetBSD.org>
2010-06-04 02:24:45 +0000

    backout the xfree86 4.5 change to GetTimeInMillis() that returns relative times instead of absolute times.  it causes problems in openmotif at the very least, and is not what xorg has done.
    from Cliff Wright in PR#39209.

--- xfree/xc/programs/Xserver/os/GetTimeInMillis.c
+++ xfree/xc/programs/Xserver/os/GetTimeInMillis.c
@@ -5,19 +5,7 @@ CARD32
 GetTimeInMillis(void)
 {
     struct timeval  tp;
-    register CARD32 val;
-    register INT32 diff;
-    static CARD32 oldval = 0;
-    static CARD32 time = 0;
 
     X_GETTIMEOFDAY(&tp);
-    val = (tp.tv_sec * 1000) + (tp.tv_usec / 1000);
-    if (oldval) {
-	diff = val - oldval;
-	if (diff > 0)
-	    time += diff;
-    }
-    oldval = val;
-
-    return time;
+    return(tp.tv_sec * 1000) + (tp.tv_usec / 1000);
 }
