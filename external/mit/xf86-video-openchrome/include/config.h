a0cb2ee0040d08f93775808d9f2111e5d05a0e50
mrg <mrg>
2010-06-08 04:31:23 +0000

    include a config.h for openchrome as well.  part of PR#43433.

---
+++ external/mit/xf86-video-openchrome/include/config.h
@@ -0,0 +1,100 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+#include "xorg-server.h"
+
+/* Enable debug support */
+/* #undef HAVE_DEBUG */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* xextproto 7.1 available */
+#define HAVE_XEXTPROTO_71 1
+
+/* Name of package */
+#define PACKAGE "xf86-video-openchrome"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.openchrome.org/trac/report/1"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "xf86-video-openchrome"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "xf86-video-openchrome 0.2.904"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xf86-video-openchrome"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2.904"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.2.904"
+
+/* Enable DRI driver support */
+#define XF86DRI 1
+
+/* Enable developmental DRI driver support */
+#define XF86DRI_DEVEL 1
+
+/* Enable libpciaccess */
+#define XSERVER_LIBPCIACCESS 1
+
+/* Enable XVideo debug support */
+/* #undef XV_DEBUG */
+
+/* Compatibility define for older Xen */
+#define X_HAVE_XAAGETROP 1
+
+/* Compatibility define for older Xen */
+#define X_NEED_I2CSTART 1
+
+/* Compatibility define for older Xen */
+#define X_USE_LINEARFB 1
+
+/* Compatibility define for older Xen */
+#define X_USE_REGION_NULL 1
