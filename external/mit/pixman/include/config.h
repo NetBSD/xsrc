680b3224b61f0b52dc374cd6cbcc185e6869d060
wiz <wiz@NetBSD.org>
2010-09-14 07:44:47 +0000

    Update for 0.18.4.

--- external/mit/pixman/include/config.h
+++ external/mit/pixman/include/config.h
@@ -57,7 +57,7 @@
 #define PACKAGE_NAME "pixman"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pixman 0.18.2"
+#define PACKAGE_STRING "pixman 0.18.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pixman"
@@ -66,7 +66,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.18.2"
+#define PACKAGE_VERSION "0.18.4"
 
 /* enable TIMER_BEGIN/TIMER_END macros */
 /* #undef PIXMAN_TIMERS */
@@ -103,7 +103,7 @@
 /* #undef USE_VMX */
 
 /* Version number of package */
-#define VERSION "0.18.2"
+#define VERSION "0.18.4"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
