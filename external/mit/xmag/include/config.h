948fb08bfcf0236e93dcdd0a0d71554d15a51260
mrg <mrg>
2010-11-20 23:24:24 +0000

    update for xmag 1.0.4.

--- external/mit/xmag/include/config.h
+++ external/mit/xmag/include/config.h
@@ -47,13 +47,16 @@
 #define PACKAGE_NAME "xmag"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xmag 1.0.3"
+#define PACKAGE_STRING "xmag 1.0.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xmag"
 
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.3"
+#define PACKAGE_VERSION "1.0.4"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
@@ -62,10 +65,10 @@
 #define PACKAGE_VERSION_MINOR 0
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 3
+#define PACKAGE_VERSION_PATCHLEVEL 4
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.3"
+#define VERSION "1.0.4"
