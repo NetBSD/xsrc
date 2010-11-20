a241306cc60ed299b89726886c250cfb82cee90b
mrg <mrg@NetBSD.org>
2010-11-20 21:58:31 +0000

    initial import of xf86-video-vmware-11.0.3

--- external/mit/xf86-video-vmware/dist/src/svga_limits.h
+++ external/mit/xf86-video-vmware/dist/src/svga_limits.h
@@ -1,8 +1,6 @@
-/* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/vmware/svga_limits.h,v 1.1 2001/04/05 19:29:44 dawes Exp $ */
 /* **********************************************************
  * Copyright (C) 1998-2001 VMware, Inc.
  * All Rights Reserved
- * $Id: svga_limits.h,v 1.1.1.1 2008/08/02 05:18:37 mrg Exp $
  * **********************************************************/
 
 /*
