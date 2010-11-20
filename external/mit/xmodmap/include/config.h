0884d3f763da44fafa5ace5f4b21f3c0207c97be
mrg <mrg@NetBSD.org>
2010-11-20 23:24:51 +0000

    update for xmodmap 1.0.4.

--- external/mit/xmodmap/include/config.h
+++ external/mit/xmodmap/include/config.h
@@ -41,13 +41,16 @@
 #define PACKAGE_NAME "xmodmap"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xmodmap 1.0.4"
+#define PACKAGE_STRING "xmodmap 1.0.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xmodmap"
 
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.4"
+#define PACKAGE_VERSION "1.0.5"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
@@ -56,10 +59,10 @@
 #define PACKAGE_VERSION_MINOR 0
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 4
+#define PACKAGE_VERSION_PATCHLEVEL 5
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.4"
+#define VERSION "1.0.5"
