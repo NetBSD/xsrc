0c0fe42921668e200e1110001f9dcc9961196c24
mrg <mrg@NetBSD.org>
2010-11-21 05:47:44 +0000

    initial import of libXevie-1.0.3

--- external/mit/libXevie/dist/src/Xevie.c
+++ external/mit/libXevie/dist/src/Xevie.c
@@ -1,34 +1,25 @@
-/* $XdotOrg: xc/lib/Xevie/Xevie.c,v 1.2 2005/05/22 01:20:14 alanc Exp $ */
 /************************************************************
 
-Copyright 2003 Sun Microsystems, Inc.
-
-All rights reserved.
+Copyright (c) 2003, Oracle and/or its affiliates. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, and/or sell copies of the Software, and to permit persons
-to whom the Software is furnished to do so, provided that the above
-copyright notice(s) and this permission notice appear in all copies of
-the Software and that both the above copyright notice(s) and this
-permission notice appear in supporting documentation.
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
-OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY SPECIAL
-INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING
-FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
-NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
-WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+The above copyright notice and this permission notice (including the next
+paragraph) shall be included in all copies or substantial portions of the
+Software.
 
-Except as contained in this notice, the name of a copyright holder
-shall not be used in advertising or otherwise to promote the sale, use
-or other dealings in this Software without prior written authorization
-of the copyright holder.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
 
 ************************************************************/
 
@@ -53,7 +44,11 @@ static long xevie_mask = 0;
  *                                                                           *
  *****************************************************************************/
 
-static int close_display();
+static int close_display(
+    Display *		/* dpy */,
+    XExtCodes *		/* codes */
+);
+
 static /* const */ XExtensionHooks xevie_extension_hooks = {
     NULL,                               /* create_gc */
     NULL,                               /* copy_gc */
@@ -82,10 +77,10 @@ static XEXT_GENERATE_CLOSE_DISPLAY (close_display, xevie_info)
  *****************************************************************************/
 
 Status
-XevieQueryVersion(dpy, major_version_return, minor_version_return)
-    Display* dpy;
-    int* major_version_return; 
-    int* minor_version_return;
+XevieQueryVersion(
+    Display	*dpy,
+    int		*major_version_return,
+    int		*minor_version_return)
 {
     XExtDisplayInfo *info = find_display (dpy);
     xXevieQueryVersionReply rep;
@@ -112,19 +107,9 @@ XevieQueryVersion(dpy, major_version_return, minor_version_return)
     return True;
 }
 
-/* Win32 reserves 20 colormap entries for its desktop */
-#ifndef TYP_RESERVED_ENTRIES
-#define TYP_RESERVED_ENTRIES 20
-#endif
-
 Status 
-#if NeedFunctionPrototypes
 XevieStart(
     Display* dpy)
-#else
-XevieStart(dpy)
-    Display* dpy;
-#endif
 {
     XExtDisplayInfo *info = find_display (dpy);
     xXevieStartReply rep;
@@ -145,8 +130,7 @@ XevieStart(dpy)
 }
 
 Status
-XevieEnd(dpy)
-    Display* dpy;
+XevieEnd(Display *dpy)
 {
     XExtDisplayInfo *info = find_display (dpy);
     xXevieEndReply rep;
@@ -167,10 +151,10 @@ XevieEnd(dpy)
 }
 
 Status
-XevieSendEvent(dpy, event, dataType)
-    Display* dpy;
-    XEvent*  event;
-    char     dataType;
+XevieSendEvent(
+    Display	*dpy,
+    XEvent	*event,
+    int		 dataType)
 {
     xXevieSendReply rep;
     xXevieSendReq *req;
@@ -189,9 +173,9 @@ XevieSendEvent(dpy, event, dataType)
 }
 
 Status
-XevieSelectInput(dpy, event_mask)
-    Display* dpy;
-    long     event_mask;
+XevieSelectInput(
+    Display	*dpy,
+    long	 event_mask)
 {
     xXevieSelectInputReply rep;
     xXevieSelectInputReq *req;
