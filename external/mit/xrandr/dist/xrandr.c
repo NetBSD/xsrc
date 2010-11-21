ea024c7579b3776795d2a1ff654ec972795f9490
mrg <mrg@NetBSD.org>
2010-11-21 03:49:09 +0000

    merge cornflakes

--- external/mit/xrandr/dist/xrandr.c
+++ external/mit/xrandr/dist/xrandr.c
@@ -2681,7 +2681,8 @@ main (int argc, char **argv)
 
     root = RootWindow (dpy, screen);
 
-    if (!XRRQueryVersion (dpy, &major, &minor))
+    if (!XRRQueryExtension (dpy, &event_base, &error_base) ||
+        !XRRQueryVersion (dpy, &major, &minor))
     {
 	fprintf (stderr, "RandR extension missing\n");
 	exit (1);
@@ -3373,8 +3374,6 @@ main (int argc, char **argv)
     if (setit && !dryrun) status = XRRSetScreenConfigAndRate (dpy, sc,
 						   root,
 						   (SizeID) size, (Rotation) (rotation | reflection), rate, CurrentTime);
-
-    XRRQueryExtension(dpy, &event_base, &error_base);
 
     if (setit && !dryrun && status == RRSetConfigFailed) {
 	printf ("Failed to change the screen configuration!\n");
