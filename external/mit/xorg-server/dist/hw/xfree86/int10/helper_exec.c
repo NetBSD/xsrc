9897886b7abbf21e751f3dd465e5325313916646
macallan <macallan>
2011-01-06 03:27:02 +0000

    get this a little closer to working on non-x86 by: * not poking around in IO space trying to disable / enable ancient VGAs when   NO_LEGACY_VGA is defined * actually checking if what we get from read_legacy_video_BIOS() is actually   a valid PC ROM image every time we try

--- external/mit/xorg-server/dist/hw/xfree86/int10/helper_exec.c
+++ external/mit/xorg-server/dist/hw/xfree86/int10/helper_exec.c
@@ -650,6 +650,7 @@ bios_checksum(const CARD8 *start, int size)
 void
 LockLegacyVGA(xf86Int10InfoPtr pInt, legacyVGAPtr vga)
 {
+#ifndef NO_LEGACY_VGA
     vga->save_msr    = inb(pInt->ioBase + 0x03CC);
     vga->save_vse    = inb(pInt->ioBase + 0x03C3);
 #ifndef __ia64__
@@ -662,17 +663,20 @@ LockLegacyVGA(xf86Int10InfoPtr pInt, legacyVGAPtr vga)
     outb(pInt->ioBase + 0x46E8, ~(CARD8)0x08 & vga->save_46e8);
 #endif
     outb(pInt->ioBase + 0x0102, ~(CARD8)0x01 & vga->save_pos102);
+#endif
 }
 
 void
 UnlockLegacyVGA(xf86Int10InfoPtr pInt, legacyVGAPtr vga)
 {
+#ifndef NO_LEGACY_VGA
     outb(pInt->ioBase + 0x0102, vga->save_pos102);
 #ifndef __ia64__
     outb(pInt->ioBase + 0x46E8, vga->save_46e8);
 #endif
     outb(pInt->ioBase + 0x03C3, vga->save_vse);
     outb(pInt->ioBase + 0x03C2, vga->save_msr);
+#endif
 }
 
 #if defined (_PC)
