246b9509c5194d71be1d6237c3877cd83e398205
ahoka <ahoka@NetBSD.org>
2011-01-22 18:57:36 +0000

    add an ifndef __linux__ requested by mrg

--- external/mit/xf86-video-intel/dist/src/i830_bios.c
+++ external/mit/xf86-video-intel/dist/src/i830_bios.c
@@ -238,7 +238,12 @@ i830_bios_init(ScrnInfoPtr pScrn)
     if (ret != 0) {
 	xf86DrvMsg(pScrn->scrnIndex, X_WARNING,
                   "libpciaccess failed to read %dkB video BIOS: %s\n",
-                  size / 1024, strerror(ret));
+                  size / 1024,
+#ifndef __linux__
+	          strerror(ret));
+#else
+                  strerror(-ret));
+#endif
 	xfree (bios);
 	return -1;
     }
