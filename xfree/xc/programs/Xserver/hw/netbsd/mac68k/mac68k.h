665c2b61b2c26a070766c5ffd8f349c52df63cf7
christos <christos@NetBSD.org>
2011-05-23 02:40:13 +0000

    fix mouse prototype

--- xfree/xc/programs/Xserver/hw/netbsd/mac68k/mac68k.h
+++ xfree/xc/programs/Xserver/hw/netbsd/mac68k/mac68k.h
@@ -94,7 +94,7 @@ extern miPointerScreenFuncRec mac_mousefuncs;
 
 
 void mac68k_processkbd(DeviceIntPtr, adb_event_t *);
-void mac68k_processmouse(DeviceIntPtr, adb_event_t *);
+void mac68k_processmouse(DevicePtr, adb_event_t *);
 void mac68k_getmouse(void);
 void mac68k_getkbd(void);
 Bool mac68k_screeninit(ScreenPtr);
