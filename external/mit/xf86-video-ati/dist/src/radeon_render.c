554255c034af8ba39857cf64e986f99324c55576
macallan <macallan@NetBSD.org>
2011-07-06 03:43:41 +0000

    make Xrender acceleration work again on R1xx tested on a Sun XVR-100

--- external/mit/xf86-video-ati/dist/src/radeon_render.c
+++ external/mit/xf86-video-ati/dist/src/radeon_render.c
@@ -409,6 +409,9 @@ static Bool FUNC_NAME(R100SetupTexture)(
     dst_pitch = RADEON_ALIGN(width * tex_bytepp, 64);
     size = dst_pitch * height;
 
+    info->accel_state->texW[0] = width;
+    info->accel_state->texH[0] = height;
+
     if ((flags & XAA_RENDER_REPEAT) && (height != 1) &&
 	(RADEON_ALIGN(width * tex_bytepp, 32) != dst_pitch))
 	return FALSE;
@@ -432,7 +435,7 @@ static Bool FUNC_NAME(R100SetupTexture)(
 	txformat |= ATILog2(width) << RADEON_TXFORMAT_WIDTH_SHIFT;
 	txformat |= ATILog2(height) << RADEON_TXFORMAT_HEIGHT_SHIFT;
     } else {
-	tex_size = (height << 16) | width;
+	tex_size = ((height - 1) << 16) | (width - 1);
 	txformat |= RADEON_TXFORMAT_NON_POWER2;
     }
 
@@ -462,7 +465,6 @@ static Bool FUNC_NAME(R100SetupTexture)(
 
     if (info->accel_state->accel->NeedToSync)
 	info->accel_state->accel->Sync(pScrn);
-
     while (height--) {
 	memcpy(dst, src, width * tex_bytepp);
 	src += src_pitch;
@@ -633,10 +635,10 @@ FUNC_NAME(R100SubsequentCPUToScreenTexture) (
 
     r = width + l;
     b = height + t;
-    fl = srcx;
-    fr = srcx + width;
-    ft = srcy;
-    fb = srcy + height;
+    fl = (float)srcx / info->accel_state->texW[0];
+    fr = (float)(srcx + width) / info->accel_state->texW[0];
+    ft = (float)srcy / info->accel_state->texH[0];
+    fb = (float)(srcy + height) / info->accel_state->texH[0];
 
 #ifdef ACCEL_CP
     BEGIN_RING(25);
