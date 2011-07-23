7fa3f82e84ac46a8b2336ccf8b5f7dd83bedcf22
mrg <mrg@NetBSD.org>
2011-07-23 09:31:53 +0000

    update for 1.7.5.

--- external/mit/xf86-video-siliconmotion/include/config.h
+++ external/mit/xf86-video-siliconmotion/include/config.h
@@ -49,13 +49,13 @@
 #define PACKAGE_NAME "xf86-video-siliconmotion"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xf86-video-siliconmotion 1.7.4"
+#define PACKAGE_STRING "xf86-video-siliconmotion 1.7.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xf86-video-siliconmotion"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.7.4"
+#define PACKAGE_VERSION "1.7.5"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
@@ -64,10 +64,10 @@
 #define PACKAGE_VERSION_MINOR 7
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 4
+#define PACKAGE_VERSION_PATCHLEVEL 5
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.7.4"
+#define VERSION "1.7.5"
