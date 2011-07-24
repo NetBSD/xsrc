d4d3ccb90d6890fa13c4afedc733ea90d1d4b5c9
mrg <mrg@NetBSD.org>
2011-07-24 12:26:14 +0000

    avoid a C construct lint complains about.

--- external/mit/libdrm/dist/radeon/radeon_cs_gem.c
+++ external/mit/libdrm/dist/radeon/radeon_cs_gem.c
@@ -517,9 +517,10 @@ static struct radeon_cs_funcs radeon_cs_gem_funcs = {
 
 static int radeon_get_device_id(int fd, uint32_t *device_id)
 {
-    struct drm_radeon_info info = {};
+    struct drm_radeon_info info;
     int r;
 
+    memset(&info, 0, sizeof info);
     *device_id = 0;
     info.request = RADEON_INFO_DEVICE_ID;
     info.value = (uintptr_t)device_id;
