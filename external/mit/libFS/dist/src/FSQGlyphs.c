07cd4da9c7795838feefd9d8745b3c395c408f6c
mrg <mrg@NetBSD.org>
2013-05-30 20:23:53 +0000

    initial import of libFS-1.0.5

--- external/mit/libFS/dist/src/FSQGlyphs.c
+++ external/mit/libFS/dist/src/FSQGlyphs.c
@@ -2,24 +2,24 @@
  * Copyright 1990 Network Computing Devices;
  * Portions Copyright 1987 by Digital Equipment Corporation
  *
- * Permission to use, copy, modify, distribute, and sell this software 
- * and its documentation for any purpose is hereby granted without fee, 
- * provided that the above copyright notice appear in all copies and 
- * that both that copyright notice and this permission notice appear 
- * in supporting documentation, and that the names of Network Computing 
- * Devices or Digital not be used in advertising or publicity pertaining 
- * to distribution of the software without specific, written prior 
- * permission. Network Computing Devices or Digital make no representations 
- * about the suitability of this software for any purpose.  It is provided 
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation, and that the names of Network Computing
+ * Devices or Digital not be used in advertising or publicity pertaining
+ * to distribution of the software without specific, written prior
+ * permission. Network Computing Devices or Digital make no representations
+ * about the suitability of this software for any purpose.  It is provided
  * "as is" without express or implied warranty.
  *
  * NETWORK COMPUTING DEVICES AND  DIGITAL DISCLAIM ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF 
+ * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL NETWORK COMPUTING DEVICES
- * OR DIGITAL BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES 
- * OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, 
- * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, 
- * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS 
+ * OR DIGITAL BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES
+ * OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
  * SOFTWARE.
  */
 
@@ -87,27 +87,27 @@ FSQueryXBitmaps8(
 	return FSBadAlloc;
 
 #if SIZE_MAX <= UINT_MAX
-    if (reply.num_chars > SIZE_MAX / sizeof(FSOffset)) 
+    if (reply.num_chars > SIZE_MAX / sizeof(FSOffset))
 	return FSBadAlloc;
 #endif
 
-    offs = (FSOffset *) FSmalloc(sizeof(FSOffset) * reply.num_chars);
+    offs = FSmalloc(sizeof(FSOffset) * reply.num_chars);
     *offsets = offs;
     if (!offs)
 	return FSBadAlloc;
 #if (SIZE_MAX >> 2) <= UINT_MAX
     /* XXX This thest is incomplete */
     if (reply.length > (SIZE_MAX >> 2)) {
-	FSfree((char *) offs);
+	FSfree(offs);
 	return FSBadAlloc;
     }
 #endif
     left = (reply.length << 2) - SIZEOF(fsQueryXBitmaps8Reply)
 	- (SIZEOF(fsOffset32) * reply.num_chars);
-    gd = (unsigned char *) FSmalloc(left);
+    gd = FSmalloc(left);
     *glyphdata = gd;
     if (!gd) {
-	FSfree((char *) offs);
+	FSfree(offs);
 	return FSBadAlloc;
     }
     for (i=0; i<reply.num_chars; i++)
@@ -150,13 +150,11 @@ FSQueryXBitmaps16(
     req->length += ((str_len * SIZEOF(fsChar2b)) + 3) >> 2;
     if (FSProtocolVersion(svr) == 1)
     {
-	int i;
 	fsChar2b_version1 *swapped_str;
 
-	if (str_len > SIZE_MAX/SIZEOF(fsChar2b_version1)) 
+	if (str_len > SIZE_MAX/SIZEOF(fsChar2b_version1))
 	    return FSBadAlloc;
-	swapped_str = (fsChar2b_version1 *)
-	    FSmalloc(SIZEOF(fsChar2b_version1) * str_len);
+	swapped_str = FSmalloc(SIZEOF(fsChar2b_version1) * str_len);
 	if (!swapped_str)
 	    return FSBadAlloc;
 	for (i = 0; i < str_len; i++) {
@@ -178,23 +176,23 @@ FSQueryXBitmaps16(
     if(reply.num_chars > SIZE_MAX/sizeof(FSOffset))
        return FSBadAlloc;
 #endif
-    offs = (FSOffset *) FSmalloc(sizeof(FSOffset) * reply.num_chars);
+    offs = FSmalloc(sizeof(FSOffset) * reply.num_chars);
     *offsets = offs;
     if (!offs)
 	return FSBadAlloc;
 #if (SIZE_MAX >> 2) <= UINT_MAX
     /* XXX - this test is incomplete */
     if (reply.length > (SIZE_MAX>>2)) {
-	FSfree((char *) offs);
+	FSfree(offs);
 	return FSBadAlloc;
     }
 #endif
     left = (reply.length << 2) - SIZEOF(fsQueryXBitmaps16Reply)
 	- (SIZEOF(fsOffset32) * reply.num_chars);
-    gd = (unsigned char *) FSmalloc(left);
+    gd = FSmalloc(left);
     *glyphdata = gd;
     if (!gd) {
-	FSfree((char *) offs);
+	FSfree(offs);
 	return FSBadAlloc;
     }
     for (i=0; i<reply.num_chars; i++)
