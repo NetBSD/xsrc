9c468974fb7759d2645474ca868d995c672338fd
mrg <mrg>
2013-05-31 01:49:13 +0000

    merge xfs 1.1.3

--- external/mit/xfs/include/config.h
+++ external/mit/xfs/include/config.h
@@ -1,132 +1,133 @@
+/* xfs-config.h.  Generated from xfs-config.h.in by configure.  */
 /* xfs-config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if `struct sockaddr_in' has a `sin_len' member */
-#undef BSD44SOCKETS
+#define BSD44SOCKETS 1
 
 /* comma-separated list of strings for config file paths when not specified */
-#undef DEFAULT_CONFIG_FILE
+#ifndef DEFAULT_CONFIG_FILE
+#define DEFAULT_CONFIG_FILE "/etc/X11/fs/config"
+#endif
 
 /* Define to 1 if you have the `daemon' function. */
-#undef HAVE_DAEMON
+#define HAVE_DAEMON 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `ws2_32' library (-lws2_32). */
-#undef HAVE_LIBWS2_32
+/* #undef HAVE_LIBWS2_32 */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if the system has the type `socklen_t'. */
-#undef HAVE_SOCKLEN_T
+#define HAVE_SOCKLEN_T 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Support IPv6 for TCP connections */
-#undef IPv6
+#define IPv6 1
 
 /* Support os-specific local connections */
-#undef LOCALCONN
+/* #undef LOCALCONN */
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "xfs"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=xorg"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "xfs"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "xfs 1.1.3"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
-
-/* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_TARNAME "xfs"
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "1.1.3"
 
 /* Major version of this package */
-#undef PACKAGE_VERSION_MAJOR
+#define PACKAGE_VERSION_MAJOR 1
 
 /* Minor version of this package */
-#undef PACKAGE_VERSION_MINOR
+#define PACKAGE_VERSION_MINOR 1
 
 /* Patch version of this package */
-#undef PACKAGE_VERSION_PATCHLEVEL
+#define PACKAGE_VERSION_PATCHLEVEL 3
 
 /* Define as the return type of signal handlers (`int' or `void'). */
-#undef RETSIGTYPE
+#define RETSIGTYPE void
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Support TCP socket connections */
-#undef TCPCONN
+#define TCPCONN 1
 
 /* Support UNIX socket connections */
-#undef UNIXCONN
+#define UNIXCONN 1
 
 /* Build support for logging via syslog */
-#undef USE_SYSLOG
-
-/* Enable extensions on AIX 3, Interix.  */
-#ifndef _ALL_SOURCE
-# undef _ALL_SOURCE
-#endif
-/* Enable GNU extensions on systems that have them.  */
-#ifndef _GNU_SOURCE
-# undef _GNU_SOURCE
-#endif
-/* Enable threading extensions on Solaris.  */
-#ifndef _POSIX_PTHREAD_SEMANTICS
-# undef _POSIX_PTHREAD_SEMANTICS
-#endif
-/* Enable extensions on HP NonStop.  */
-#ifndef _TANDEM_SOURCE
-# undef _TANDEM_SOURCE
-#endif
-/* Enable general extensions on Solaris.  */
-#ifndef __EXTENSIONS__
-# undef __EXTENSIONS__
-#endif
-
+#define USE_SYSLOG 1
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "1.1.3"
 
 /* Build support for starting from inetd */
-#undef XFS_INETD
+#define XFS_INETD 1
+
+/* Define to 1 if on AIX 3.
+   System headers sometimes define this.
+   We just want to avoid a redefinition error message.  */
+#ifndef _ALL_SOURCE
+/* # undef _ALL_SOURCE */
+#endif
+
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
 
 /* Define to 1 if on MINIX. */
-#undef _MINIX
+/* #undef _MINIX */
 
 /* Define to 2 if the system does not provide POSIX.1 features except with
    this defined. */
-#undef _POSIX_1_SOURCE
+/* #undef _POSIX_1_SOURCE */
 
 /* Define to 1 if you need to in order for `stat' and other things to work. */
-#undef _POSIX_SOURCE
+/* #undef _POSIX_SOURCE */
+
+/* Enable extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
