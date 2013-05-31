a5d139ee3042e9aa3f3ff2df3c2022d12e41e82e
mrg <mrg>
2013-05-31 06:47:44 +0000

    add generated config.h for xdpyinfo

---
+++ external/mit/xdpyinfo/include/config.h
@@ -0,0 +1,122 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <X11/extensions/dmxext.h> header file. */
+/* #undef HAVE_X11_EXTENSIONS_DMXEXT_H */
+
+/* Define to 1 if you have the <X11/extensions/multibuf.h> header file. */
+#define HAVE_X11_EXTENSIONS_MULTIBUF_H 1
+
+/* Define to 1 if you have the <X11/extensions/record.h> header file. */
+#define HAVE_X11_EXTENSIONS_RECORD_H 1
+
+/* Define to 1 if you have the <X11/extensions/Xcomposite.h> header file. */
+#define HAVE_X11_EXTENSIONS_XCOMPOSITE_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86dgaproto.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86DGAPROTO_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86dgastr.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86DGASTR_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86dga.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86DGA_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86misc.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86MISC_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86mscstr.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86MSCSTR_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86vmode.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86VMODE_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86vmproto.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86VMPROTO_H 1
+
+/* Define to 1 if you have the <X11/extensions/xf86vmstr.h> header file. */
+#define HAVE_X11_EXTENSIONS_XF86VMSTR_H 1
+
+/* Define to 1 if you have the <X11/extensions/Xinerama.h> header file. */
+#define HAVE_X11_EXTENSIONS_XINERAMA_H 1
+
+/* Define to 1 if you have the <X11/extensions/XInput.h> header file. */
+#define HAVE_X11_EXTENSIONS_XINPUT_H 1
+
+/* Define to 1 if you have the <X11/extensions/XKB.h> header file. */
+#define HAVE_X11_EXTENSIONS_XKB_H 1
+
+/* Define to 1 if you have the <X11/extensions/Xrender.h> header file. */
+#define HAVE_X11_EXTENSIONS_XRENDER_H 1
+
+/* Define to 1 if you have the <X11/extensions/XShm.h> header file. */
+#define HAVE_X11_EXTENSIONS_XSHM_H 1
+
+/* Define to 1 if you have the <X11/extensions/Xxf86dga.h> header file. */
+#define HAVE_X11_EXTENSIONS_XXF86DGA_H 1
+
+/* Define to 1 if you have the <X11/XKBlib.h> header file. */
+#define HAVE_X11_XKBLIB_H 1
+
+/* Name of package */
+#define PACKAGE "xdpyinfo"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=xorg"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "xdpyinfo"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "xdpyinfo 1.3.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xdpyinfo"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.3.1"
+
+/* Major version of this package */
+#define PACKAGE_VERSION_MAJOR 1
+
+/* Minor version of this package */
+#define PACKAGE_VERSION_MINOR 3
+
+/* Patch version of this package */
+#define PACKAGE_VERSION_PATCHLEVEL 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.3.1"
