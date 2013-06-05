be1350a15a1eba00dbe5bfc42fcc6b69338aa625
mrg <mrg@NetBSD.org>
2013-06-05 07:13:59 +0000

    udpate for 1.9.0

--- external/mit/xf86-input-mouse/include/config.h
+++ external/mit/xf86-input-mouse/include/config.h
@@ -45,7 +45,7 @@
 #define PACKAGE_NAME "xf86-input-mouse"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xf86-input-mouse 1.7.1"
+#define PACKAGE_STRING "xf86-input-mouse 1.9.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xf86-input-mouse"
@@ -54,19 +54,19 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.7.1"
+#define PACKAGE_VERSION "1.9.0"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 1
 
 /* Minor version of this package */
-#define PACKAGE_VERSION_MINOR 7
+#define PACKAGE_VERSION_MINOR 9
 
 /* Patch version of this package */
-#define PACKAGE_VERSION_PATCHLEVEL 1
+#define PACKAGE_VERSION_PATCHLEVEL 0
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.7.1"
+#define VERSION "1.9.0"
