cec4939cc9ddb0986a670bb8f1d108601aa4f90e
mrg <mrg@NetBSD.org>
2013-06-05 01:47:45 +0000

    update for xf86-video-wsfb 0.4.0

--- external/mit/xf86-video-wsfb/include/config.h
+++ external/mit/xf86-video-wsfb/include/config.h
@@ -49,19 +49,19 @@
 #define PACKAGE_NAME "xf86-video-wsfb"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xf86-video-wsfb 0.3.0"
+#define PACKAGE_STRING "xf86-video-wsfb 0.4.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xf86-video-wsfb"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.3.0"
+#define PACKAGE_VERSION "0.4.0"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 0
 
 /* Minor version of this package */
-#define PACKAGE_VERSION_MINOR 3
+#define PACKAGE_VERSION_MINOR 4
 
 /* Patch version of this package */
 #define PACKAGE_VERSION_PATCHLEVEL 0
@@ -70,4 +70,4 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.3.0"
+#define VERSION "0.4.0"
