9be3a365ed4b143a2a7c485ef6374cd096eecbbe
mrg <mrg@NetBSD.org>
2014-03-17 07:10:53 +0000

    libpthread-stubs needs a config.h

---
+++ external/mit/libpthread-stubs/include/config.h
@@ -0,0 +1,92 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `pthread_cond_broadcast' function. */
+#define HAVE_PTHREAD_COND_BROADCAST 1
+
+/* Define to 1 if you have the `pthread_cond_destroy' function. */
+#define HAVE_PTHREAD_COND_DESTROY 1
+
+/* Define to 1 if you have the `pthread_cond_init' function. */
+#define HAVE_PTHREAD_COND_INIT 1
+
+/* Define to 1 if you have the `pthread_cond_signal' function. */
+#define HAVE_PTHREAD_COND_SIGNAL 1
+
+/* Define to 1 if you have the `pthread_cond_wait' function. */
+#define HAVE_PTHREAD_COND_WAIT 1
+
+/* Define to 1 if you have the `pthread_equal' function. */
+#define HAVE_PTHREAD_EQUAL 1
+
+/* Define to 1 if you have the `pthread_mutex_destroy' function. */
+#define HAVE_PTHREAD_MUTEX_DESTROY 1
+
+/* Define to 1 if you have the `pthread_mutex_init' function. */
+#define HAVE_PTHREAD_MUTEX_INIT 1
+
+/* Define to 1 if you have the `pthread_mutex_lock' function. */
+#define HAVE_PTHREAD_MUTEX_LOCK 1
+
+/* Define to 1 if you have the `pthread_mutex_unlock' function. */
+#define HAVE_PTHREAD_MUTEX_UNLOCK 1
+
+/* Define to 1 if you have the `pthread_self' function. */
+#define HAVE_PTHREAD_SELF 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "libpthread-stubs"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "xcb@lists.freedesktop.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libpthread-stubs"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libpthread-stubs 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libpthread-stubs"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define this if the compiler supports the alias attribute */
+#define SUPPORT_ATTRIBUTE_ALIAS 1
+
+/* Version number of package */
+#define VERSION "0.1"
