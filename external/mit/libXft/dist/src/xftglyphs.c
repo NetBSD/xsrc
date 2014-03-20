12d4a5f463072438c3962f06e8a1188af44f6d50
mrg <mrg@NetBSD.org>
2014-03-20 08:58:36 +0000

    pull over pkgsrc patch patch-src_xftglyphs.c to work with modern freetype.

--- external/mit/libXft/dist/src/xftglyphs.c
+++ external/mit/libXft/dist/src/xftglyphs.c
@@ -21,10 +21,10 @@
  */
 
 #include "xftint.h"
-#include <freetype/ftoutln.h>
-#include <freetype/ftlcdfil.h>
-
-#include <freetype/ftsynth.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include FT_LCD_FILTER_H
+#include FT_SYNTHESIS_H
 
 /*
  * Validate the memory info for a font
