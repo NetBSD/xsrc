bc564669f4083560dc465830c753ea251926dbef
mrg <mrg@NetBSD.org>
2014-07-12 04:28:42 +0000

    initial import of pixman-0.32.6

--- external/mit/pixman/dist/pixman/pixman-version.h
+++ external/mit/pixman/dist/pixman/pixman-version.h
@@ -33,9 +33,9 @@
 
 #define PIXMAN_VERSION_MAJOR 0
 #define PIXMAN_VERSION_MINOR 32
-#define PIXMAN_VERSION_MICRO 4
+#define PIXMAN_VERSION_MICRO 6
 
-#define PIXMAN_VERSION_STRING "0.32.4"
+#define PIXMAN_VERSION_STRING "0.32.6"
 
 #define PIXMAN_VERSION_ENCODE(major, minor, micro) (	\
 	  ((major) * 10000)				\
