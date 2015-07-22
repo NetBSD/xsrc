41667ceabb83567afd06ca896d336f30553aafd9
mrg <mrg@NetBSD.org>
2015-07-22 08:36:10 +0000

    merge xfsinfo 1.0.5, xinit 1.3.4, xinput 1.6.1 xkbcomp 1.3.0 and xkbevd 1.1.4.

--- external/mit/xinit/dist/xinitrc.cpp
+++ external/mit/xinit/dist/xinitrc.cpp
@@ -41,49 +41,6 @@ fi
 
 XCOMM start some nice programs
 
-#if defined(__SCO__) || defined(__UNIXWARE__)
-if [ -r /etc/default/xdesktops ]; then
-  . /etc/default/xdesktops
-fi
-
-if [ -r $HOME/.x11rc ]; then
-  . $HOME/.x11rc
-else
-  if [ -r /etc/default/X11 ]; then
-  . /etc/default/X11
-  fi
-fi
-
-#if defined(__SCO__)
-if [ -n "$XSESSION" ]; then
-  case "$XSESSION" in
-    [Yy][Ee][Ss])
-      [ -x /usr/bin/X11/scosession ] && exec /usr/bin/X11/scosession
-      ;;
-  esac
-fi
-
-if [ -n "$XDESKTOP" ]; then
-  exec `eval echo $"$XDESKTOP"`
-else
-  if [ -x /usr/bin/X11/pmwm -a -x /usr/bin/X11/scoterm ]; then
-    /usr/bin/X11/scoterm 2> /dev/null &
-    exec /usr/bin/X11/pmwm    2> /dev/null
-  fi
-fi
-#elif defined(__UNIXWARE__)
-if [ -n "$XDESKTOP" ]; then
-  exec `eval echo $"$XDESKTOP"`
-else
-  if [ -x /usr/X/bin/pmwm ]; then
-    exec /usr/X/bin/pmwm    2> /dev/null
-  fi
-fi
-#endif
-
-XCOMM This is the fallback case if nothing else is executed above
-#endif /* !defined(__SCO__)  && !defined(__UNIXWARE__) */
-
 if [ -d XINITDIR/xinitrc.d ] ; then
 	for f in XINITDIR/xinitrc.d/?*.sh ; do
 		[ -x "$f" ] && . "$f"
