9b5300837ee30e7a9e9b5e1abeb347888455dac7
mrg <mrg@NetBSD.org>
2015-07-22 02:23:01 +0000

    merge libXext 1.3.3, libXi 1.7.4, libXinerama 1.1.3 and libpthread-stubs 0.3.

--- external/mit/libpthread-stubs/include/config.h
+++ external/mit/libpthread-stubs/include/config.h
@@ -10,6 +10,12 @@
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
+/* Define to 1 if you have the `pthread_condattr_destroy' function. */
+#define HAVE_PTHREAD_CONDATTR_DESTROY 1
+
+/* Define to 1 if you have the `pthread_condattr_init' function. */
+#define HAVE_PTHREAD_CONDATTR_INI 1
+
 /* Define to 1 if you have the `pthread_cond_broadcast' function. */
 #define HAVE_PTHREAD_COND_BROADCAST 1
 
@@ -22,11 +28,17 @@
 /* Define to 1 if you have the `pthread_cond_signal' function. */
 #define HAVE_PTHREAD_COND_SIGNAL 1
 
+/* Define to 1 if you have the `pthread_cond_timedwait' function. */
+#define HAVE_PTHREAD_COND_TIMEDWAIT 1
+
 /* Define to 1 if you have the `pthread_cond_wait' function. */
 #define HAVE_PTHREAD_COND_WAIT 1
 
 /* Define to 1 if you have the `pthread_equal' function. */
 #define HAVE_PTHREAD_EQUAL 1
+
+/* Define to 1 if you have the `pthread_exit' function. */
+#define HAVE_PTHREAD_EXIT 1
 
 /* Define to 1 if you have the `pthread_mutex_destroy' function. */
 #define HAVE_PTHREAD_MUTEX_DESTROY 1
@@ -64,6 +76,10 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#undef LT_OBJDIR
+
 /* Name of package */
 #define PACKAGE "libpthread-stubs"
 
@@ -74,13 +90,16 @@
 #define PACKAGE_NAME "libpthread-stubs"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libpthread-stubs 0.1"
+#define PACKAGE_STRING "libpthread-stubs 0.3"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libpthread-stubs"
 
+/* Define to the home page for this package. */
+#undef PACKAGE_URL
+
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1"
+#define PACKAGE_VERSION "0.3"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -89,4 +108,4 @@
 #define SUPPORT_ATTRIBUTE_ALIAS 1
 
 /* Version number of package */
-#define VERSION "0.1"
+#define VERSION "0.3"
