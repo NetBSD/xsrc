84fd58258f64faf95f9f181315840bd5a57065e5
wiz <wiz@NetBSD.org>
2015-09-28 20:48:46 +0000

    Remove diffs to upstream.
    No change in warnings on amd64.
    Ok christos

--- external/mit/xf86-video-s3virge/dist/src/s3v_accel.c
+++ external/mit/xf86-video-s3virge/dist/src/s3v_accel.c
@@ -35,7 +35,6 @@ in this Software without prior written authorization from the XFree86 Project.
 #include "miline.h"
 	/* fb includes are in s3v.h */
 #include "xaarop.h"
-#include <X11/Xos.h>
 
 #include "servermd.h" /* LOG2_BYTES_PER_SCANLINE_PAD */
 
