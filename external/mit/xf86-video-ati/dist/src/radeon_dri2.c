8596b31ad3d985b490c0a7ab9af431ecc45ce254
wiz <wiz@NetBSD.org>
2015-12-11 15:37:44 +0000

    Initialize a variable, for clang.

--- external/mit/xf86-video-ati/dist/src/radeon_dri2.c
+++ external/mit/xf86-video-ati/dist/src/radeon_dri2.c
@@ -590,7 +590,7 @@ static void
 radeon_dri2_client_state_changed(CallbackListPtr *ClientStateCallback, pointer data, pointer calldata)
 {
     DRI2ClientEventsPtr pClientEventsPriv;
-    DRI2FrameEventPtr ref;
+    DRI2FrameEventPtr ref = NULL;
     NewClientInfoRec *clientinfo = calldata;
     ClientPtr pClient = clientinfo->client;
     pClientEventsPriv = GetDRI2ClientEvents(pClient);
