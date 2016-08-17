23439392a759e0796cf00d37b88c27d8b716692f
mrg <mrg@NetBSD.org>
2016-08-17 00:09:45 +0000

    update for xf86-video-ati 7.7.0

--- external/mit/xf86-video-ati-kms/include/config.h
+++ external/mit/xf86-video-ati-kms/include/config.h
@@ -9,6 +9,18 @@
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
+/* Define to 1 if you have the <dri3.h> header file. */
+#undef HAVE_DRI3_H
+
+/* Have fbGlyphs API */
+#undef HAVE_FBGLYPHS
+
+/* Have glamor_egl_destroy_textured_pixmap API */
+#undef HAVE_GLAMOR_EGL_DESTROY_TEXTURED_PIXMAP
+
+/* Have glamor_glyphs_init API */
+#undef HAVE_GLAMOR_GLYPHS_INIT
+
 /* Define to 1 if you have the <glamor.h> header file. */
 /* #undef HAVE_GLAMOR_H */
 
@@ -18,11 +30,14 @@
 /* libudev support */
 /* #undef HAVE_LIBUDEV */
 
-/* Define to 1 if you have the <list.h> header file. */
-#define HAVE_LIST_H 1
-
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <misyncshm.h> header file. */
+/* #define HAVE_MISYNCSHM_H 1 */
+
+/* Define to 1 if you have the <present.h> header file. */
+/* #define HAVE_PRESENT_H 1 */
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -48,11 +63,14 @@
 /* xextproto 7.1 available */
 #define HAVE_XEXTPROTO_71 1
 
+// XXXMRG
+/* Have xf86CursorResetCursor API */
+#undef HAVE_XF86_CURSOR_RESET_CURSOR
+
 /* Have xorg_list API */
 /* #undef HAVE_XORG_LIST */
 
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
 #define LT_OBJDIR ".libs/"
 
 /* Define to 1 if your C compiler doesn't accept -c and -o together. */
@@ -68,7 +86,7 @@
 #define PACKAGE_NAME "xf86-video-ati"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xf86-video-ati 7.5.0"
+#define PACKAGE_STRING "xf86-video-ati 7.7.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xf86-video-ati"
@@ -77,13 +95,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.5.0"
+#define PACKAGE_VERSION "7.7.0"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 7
 
 /* Minor version of this package */
-#define PACKAGE_VERSION_MINOR 5
+#define PACKAGE_VERSION_MINOR 7
 
 /* Patch version of this package */
 #define PACKAGE_VERSION_PATCHLEVEL 0
@@ -98,7 +116,7 @@
 #define USE_SYS_ENDIAN_H 1
 
 /* Version number of package */
-#define VERSION "7.5.0"
+#define VERSION "7.7.0"
 
 /* Enable large inode numbers on Mac OS X 10.5.  */
 #ifndef _DARWIN_USE_64_BIT_INODE
