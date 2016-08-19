3a65919b5ad4b3f58693ad9d7e1a32f93644f73d
mrg <mrg@NetBSD.org>
2016-08-19 03:39:51 +0000

    add a config.h for amdgpu driver

---
+++ external/mit/xf86-video-amdgpu/include/config.h
@@ -0,0 +1,138 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+#include "xorg-server.h"
+
+/* Define to 1 if you have the <byteswap.h> header file. */
+/* #undef HAVE_BYTESWAP_H */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <dri3.h> header file. */
+#define HAVE_DRI3_H 1
+
+/* Have fbGlyphs API */
+/* #define HAVE_FBGLYPHS 1 */
+
+/* Have GBM_BO_USE_LINEAR */
+/* #define HAVE_GBM_BO_USE_LINEAR 1 */
+
+/* Have glamor_egl_destroy_textured_pixmap API */
+/* #define HAVE_GLAMOR_EGL_DESTROY_TEXTURED_PIXMAP 1 */
+
+/* Have glamor_glyphs_init API */
+/* #undef HAVE_GLAMOR_GLYPHS_INIT */
+
+/* Define to 1 if you have the <glamor.h> header file. */
+#define HAVE_GLAMOR_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* libudev support */
+/* #undef HAVE_LIBUDEV */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <misyncshm.h> header file. */
+#define HAVE_MISYNCSHM_H 1
+
+/* Define to 1 if you have the <present.h> header file. */
+#define HAVE_PRESENT_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* xextproto 7.1 available */
+#define HAVE_XEXTPROTO_71 1
+
+/* Have xf86CursorResetCursor API */
+#define HAVE_XF86_CURSOR_RESET_CURSOR 1
+
+/* Have xorg_list API */
+#define HAVE_XORG_LIST 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "xf86-video-amdgpu"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://bugs.freedesktop.org/enter_bug.cgi?product=xorg"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "xf86-video-amdgpu"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "xf86-video-amdgpu 1.1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xf86-video-amdgpu"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1.0"
+
+/* Major version of this package */
+#define PACKAGE_VERSION_MAJOR 1
+
+/* Minor version of this package */
+#define PACKAGE_VERSION_MINOR 1
+
+/* Patch version of this package */
+#define PACKAGE_VERSION_PATCHLEVEL 0
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Enable glamor acceleration */
+#define USE_GLAMOR 1
+
+/* Define to use byteswap macros from <sys/endian.h> */
+#define USE_SYS_ENDIAN_H 1
+
+/* Version number of package */
+#define VERSION "1.1.0"
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
+
+/* Define to 16-bit byteswap macro */
+#define bswap_16 bswap16
+
+/* Define to 32-bit byteswap macro */
+#define bswap_32 bswap32
+
+/* Define to 64-bit byteswap macro */
+#define bswap_64 bswap64
