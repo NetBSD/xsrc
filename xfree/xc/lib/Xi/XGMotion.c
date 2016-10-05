395afe5f636d86129b1c44f704cd985389780bf5
bouyer <bouyer@NetBSD.org>
2016-10-05 09:50:09 +0000

    Apply patch, requested my mrg in ticket 1263: 	xsrc/xfree/xc/lib/X11/FontNames.c		patch 	xsrc/xfree/xc/lib/X11/GetImage.c		patch 	xsrc/xfree/xc/lib/X11/ListExt.c			patch 	xsrc/xfree/xc/lib/X11/ModMap.c			patch 	xsrc/xfree/xc/lib/X11/Xlibint.h			patch 	xsrc/xfree/xc/lib/Xi/XGMotion.c			patch 	xsrc/xfree/xc/lib/Xi/XGetBMap.c			patch 	xsrc/xfree/xc/lib/Xi/XGetDCtl.c			patch 	xsrc/xfree/xc/lib/Xi/XGetFCtl.c			patch 	xsrc/xfree/xc/lib/Xi/XGetKMap.c			patch 	xsrc/xfree/xc/lib/Xi/XGetMMap.c			patch 	xsrc/xfree/xc/lib/Xi/XOpenDev.c			patch 	xsrc/xfree/xc/lib/Xi/XQueryDv.c			patch 	xsrc/xfree/xc/lib/Xrender/Filter.c		patch 	xsrc/xfree/xc/lib/Xrender/Xrender.c		patch 	xsrc/xfree/xc/lib/Xtst/XRecord.c		patch 	xsrc/xfree/xc/lib/Xv/Xv.c			patch 	xsrc/xfree/xc/programs/Xserver/include/dix.h	patch
    Fix (backported from upstream) the following issues in X client
    libraries:
    libX11 - insufficient validation of data from the X server
    	 can cause out of boundary memory read (XGetImage())
    	 or write (XListFonts()).
    	 Affected versions libX11 <= 1.6.3
    
    libXfixes - insufficient validation of data from the X server
    	can cause an integer overflow on 32 bit architectures.
    	Affected versions : libXfixes <= 5.0.2
    
    libXi - insufficient validation of data from the X server
    	can cause out of boundary memory access or
    	endless loops (Denial of Service).
    	Affected versions libXi <= 1.7.6
    
    libXrandr - insufficient validation of data from the X server
    	can cause out of boundary memory writes.
    	Affected versions: libXrandr <= 1.5.0
    
    libXrender - insufficient validation of data from the X server
    	can cause out of boundary memory writes.
    	Affected version: libXrender <= 0.9.9
    
    XRecord - insufficient validation of data from the X server
    	can cause out of boundary memory access or
    	endless loops (Denial of Service).
    	 Affected version libXtst <= 1.2.2
    
    libXv - insufficient validation of data from the X server
    	can cause out of boundary memory and memory corruption.
    	CVE-2016-5407
    	affected versions libXv <= 1.0.10
    
    libXvMC - insufficient validation of data from the X server
    	can cause a one byte buffer read underrun.
    	Affected versions: libXvMC <= 1.0.9

--- xfree/xc/lib/Xi/XGMotion.c
+++ xfree/xc/lib/Xi/XGMotion.c
@@ -59,6 +59,7 @@ SOFTWARE.
 #include <X11/extensions/XInput.h>
 #include <X11/extensions/extutil.h>
 #include "XIint.h"
+#include <limits.h>
 
 XDeviceTimeCoord
 *XGetDeviceMotionEvents (dpy, dev, start, stop, nEvents, mode, axis_count)
@@ -74,7 +75,7 @@ XDeviceTimeCoord
     xGetDeviceMotionEventsReply 	rep;
     XDeviceTimeCoord *tc;
     int *data, *bufp, *readp, *savp;
-    long size, size2;
+    long size;
     int	 i, j;
     XExtDisplayInfo *info = XInput_find_display (dpy);
 
@@ -105,11 +106,23 @@ XDeviceTimeCoord
         SyncHandle();
 	return (NULL);
 	}
-    size = rep.length << 2;
-    size2 = rep.nEvents * 
-	(sizeof (XDeviceTimeCoord) + (rep.axes * sizeof (int)));
-    savp = readp = (int *) Xmalloc (size);
-    bufp = (int *) Xmalloc (size2);
+    if (rep.length < (INT_MAX >> 2)) {
+	size = rep.length << 2;
+	savp = readp = Xmalloc(size);
+    } else {
+	size = 0;
+	savp = readp = NULL;
+    }
+    /* rep.axes is a CARD8, so assume max number of axes for bounds check */
+    if (rep.nEvents <
+        (INT_MAX / (sizeof(XDeviceTimeCoord) + (UCHAR_MAX * sizeof(int)))) &&
+        rep.nEvents * (rep.axes + 1) <= rep.length) {
+        size_t bsize = rep.nEvents *
+            (sizeof(XDeviceTimeCoord) + (rep.axes * sizeof(int)));
+        bufp = Xmalloc(bsize);
+    } else
+        bufp = NULL;
+
     if (!bufp || !savp)
 	{
 	*nEvents = 0;
