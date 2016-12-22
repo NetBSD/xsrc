6478913627b65d8f3394638f9590b2998c54b3ab
macallan <macallan@NetBSD.org>
2016-12-22 21:31:08 +0000

    fix tpyo - now fallback to ARGB masks when rendering glyphs actually works

--- external/mit/xorg-server/dist/exa/exa_glyphs.c
+++ external/mit/xorg-server/dist/exa/exa_glyphs.c
@@ -726,7 +726,7 @@ exaGlyphs(CARD8 op,
                               serverClient, &error);
         if (!pMask ||
             (!component_alpha && pExaScr->info->CheckComposite &&
-             !(*pExaScr->info->CheckComposite) (PictOpAdd, pSrc, NULL, pMask)))
+             !(*pExaScr->info->CheckComposite) (PictOpAdd, pSrc, pMask, NULL)))
         {
             PictFormatPtr argbFormat;
 
