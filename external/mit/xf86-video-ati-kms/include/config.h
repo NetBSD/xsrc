f994f6bba5ea3f396f4d1f09e7acd1537b0e1b8a
mrg <mrg@NetBSD.org>
2017-03-05 03:39:46 +0000

    update properly for xf86-vide-ati 7.8.0.

--- external/mit/xf86-video-ati-kms/include/config.h
+++ external/mit/xf86-video-ati-kms/include/config.h
@@ -89,7 +89,7 @@
 #define PACKAGE_NAME "xf86-video-ati"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "xf86-video-ati 7.7.0"
+#define PACKAGE_STRING "xf86-video-ati 7.8.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "xf86-video-ati"
@@ -98,13 +98,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "7.7.0"
+#define PACKAGE_VERSION "7.8.0"
 
 /* Major version of this package */
 #define PACKAGE_VERSION_MAJOR 7
 
 /* Minor version of this package */
-#define PACKAGE_VERSION_MINOR 7
+#define PACKAGE_VERSION_MINOR 8
 
 /* Patch version of this package */
 #define PACKAGE_VERSION_PATCHLEVEL 0
@@ -119,7 +119,7 @@
 #define USE_SYS_ENDIAN_H 1
 
 /* Version number of package */
-#define VERSION "7.7.0"
+#define VERSION "7.8.0"
 
 /* Enable large inode numbers on Mac OS X 10.5.  */
 #ifndef _DARWIN_USE_64_BIT_INODE
