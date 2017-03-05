3072cf756443b2643958f583b95287b9ad574e9f
mrg <mrg@NetBSD.org>
2017-03-05 01:19:56 +0000

    initial import of xf86-video-r128-6.10.2

--- external/mit/xf86-video-r128/dist/src/r128_output.c
+++ external/mit/xf86-video-r128/dist/src/r128_output.c
@@ -35,7 +35,13 @@
 
 #include "xf86.h"
 #include "xf86Modes.h"
+
+#ifdef HAVE_XEXTPROTO_71
 #include "X11/extensions/dpmsconst.h"
+#else
+#define DPMS_SERVER
+#include "X11/extensions/dpms.h"
+#endif
 
 #include "r128.h"
 #include "r128_probe.h"
