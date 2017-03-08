74a432e1c7d298cb1ae126296a361d40ff6d619e
mrg <mrg@NetBSD.org>
2017-03-08 07:44:16 +0000

    port to xorg-server 1.10:
    apply
    https://cgit.freedesktop.org/xorg/xserver/commit/?id=957e8db38f27932d353e86e9aa69cf16778b18f1
    
    --
    Use arc4random_buf(3) if available to generate cookies.
    
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Signed-off-by: Matthieu Herrb <matthieu@herrb.eu>
    --

--- external/mit/xorg-server.old/include/dix-config.h
+++ external/mit/xorg-server.old/include/dix-config.h
@@ -134,6 +134,9 @@
 /* Build a standalone xpbproxy */
 /* #undef STANDALONE_XPBPROXY */
 
+/* Define to 1 if you have the `bsd' library (-lbsd). */
+/* #undef HAVE_LIBBSD */
+
 /* Define to 1 if you have the `m' library (-lm). */
 #define HAVE_LIBM 1
 
@@ -160,6 +163,9 @@
 
 /* Define to 1 if you have the <rpcsvc/dbm.h> header file. */
 /* #undef HAVE_RPCSVC_DBM_H */
+
+/* Define to 1 if you have the `arc4random_buf' function. */
+#define HAVE_ARC4RANDOM_BUF 1
 
 /* Define to use libc SHA1 functions */
 #define HAVE_SHA1_IN_LIBC 1
