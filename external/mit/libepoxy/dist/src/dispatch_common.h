8c7a05682863c48e2a2e3b1f96115261628e05e0
mrg <mrg@NetBSD.org>
2019-07-10 07:10:48 +0000

    fix conditional to avoid redefinition.

--- external/mit/libepoxy/dist/src/dispatch_common.h
+++ external/mit/libepoxy/dist/src/dispatch_common.h
@@ -36,7 +36,7 @@
 #define PLATFORM_HAS_GLX 0
 #define PLATFORM_HAS_WGL 0
 #elif defined(__NetBSD__)
-# if defined(PLATFORM_HAS_EGL)
+# if !defined(PLATFORM_HAS_EGL)
 #  if defined(__amd64__) || defined(__i386__) || defined(__aarch64__) // XXX evbarm32
 #   define PLATFORM_HAS_EGL 1
 #  else
