18355f5dc4613c52570a9061eb79b07da5996434
martin <martin@NetBSD.org>
2020-07-26 10:49:51 +0000

    Pull up following revision(s) (requested by tsutsui in ticket #1026):
    	external/mit/xorg-server.old/dist/hw/netbsd/x68k/x68kInit.c: revision 1.3
    
    Use AllocDevicePair() to initialize input devices in InitInput().
    
    This should have been done on the initial X11R7'fied imoprt as
    upstream xnest did:
     https://cgit.freedesktop.org/xorg/xserver/commit?id=32ce5c83a504d2cafd2b19ed628b40032ef347b3
    
    Tested and no visible regression on XM6i.

--- external/mit/xorg-server.old/dist/hw/netbsd/x68k/x68kInit.c
+++ external/mit/xorg-server.old/dist/hw/netbsd/x68k/x68kInit.c
@@ -1,4 +1,4 @@
-/* $NetBSD: x68kInit.c,v 1.1.1.1 2016/06/09 09:07:59 mrg Exp $ */
+/* $NetBSD: x68kInit.c,v 1.1.1.1.4.1 2020/07/26 10:49:51 martin Exp $ */
 /*-------------------------------------------------------------------------
  * Copyright (c) 1996 Yasushi Yamasaki
  * All rights reserved.
@@ -133,8 +133,14 @@ InitOutput(ScreenInfo *pScreenInfo, int argc, char *argv[])
 void
 InitInput(int argc, char *argv[]) 
 {
-    x68kPointerDevice = AddInputDevice(serverClient, x68kMouseProc, TRUE);
-    x68kKeyboardDevice = AddInputDevice(serverClient, x68kKbdProc, TRUE);
+    int rc;
+
+    rc = AllocDevicePair(serverClient, "x68k",
+			 &x68kPointerDevice, &x68kKeyboardDevice,
+			 x68kMouseProc,x68kKbdProc, FALSE);
+    if (rc != Success)
+	FatalError("Failed to init x68k default input devices.\n");
+
     GetEventList(&x68kEvents);
 
     if ( !mieqInit() )
