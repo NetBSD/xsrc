6057a846732de108df74a69c2fc227b48202105d
macallan <macallan@NetBSD.org>
2021-11-12 19:01:19 +0000

    fix license

--- external/mit/xf86-video-mgx/dist/src/mgx.h
+++ external/mit/xf86-video-mgx/dist/src/mgx.h
@@ -1,7 +1,7 @@
 /*
- * TCX framebuffer - defines.
+ * Southland Media MGX - defines and such.
  *
- * Copyright (C) 2000 Jakub Jelinek (jakub@redhat.com)
+ * Copyright (C) 2021 Michael Lorenz
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -16,7 +16,7 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * JAKUB JELINEK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * MICHAEL LORENZ BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
