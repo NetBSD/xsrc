004db1f135e237359024f9aef1a03542e8716e2c
mrg <mrg@NetBSD.org>
2022-07-12 01:13:15 +0000

    updates for xfs 1.2.1, and xdpyinfo 1.3.3

--- external/mit/xdpyinfo/include/config.h
+++ external/mit/xdpyinfo/include/config.h
@@ -4,11 +4,11 @@
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
@@ -115,7 +115,9 @@
 /* Patch version of this package */
 #define PACKAGE_VERSION_PATCHLEVEL 2
 
-/* Define to 1 if you have the ANSI C header files. */
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
