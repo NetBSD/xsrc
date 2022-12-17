a1d8a91d7808cbdb9f81a9b464062c923cc31f0d
mrg <mrg@NetBSD.org>
2022-12-17 09:06:34 +0000

    un-merge pixman 0.42.2 and re-merge 0.38.4.

--- external/mit/pixman/dist/pixman/pixman-inlines.h
+++ external/mit/pixman/dist/pixman/pixman-inlines.h
@@ -231,7 +231,7 @@ bilinear_interpolation_float (argb_t tl, argb_t tr,
     argb_t r;
 
     distxy = distx * disty;
-    distxiy = distx * (1.f - disty);
+    distxiy = distx - (1.f - distxy);
     distixy = (1.f - distx) * disty;
     distixiy = (1.f - distx) * (1.f - disty);
 
