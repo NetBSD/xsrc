7f60d2b9a4c7f194f1daabf0909b3d8be5942737
riastradh <riastradh@NetBSD.org>
2023-06-04 11:36:50 +0000

    mesa: Remove massive kludge that worked around PR toolchain/50277.
    Now that the underlying bug in ld.elf_so is fixed, the workaround is
    causing harm.
    
    XXX pullup-10

--- external/mit/MesaLib.old/dist/src/glx/glxcurrent.c
+++ external/mit/MesaLib.old/dist/src/glx/glxcurrent.c
@@ -40,18 +40,6 @@
 #include "glx_error.h"
 
 /*
- * MASSIVE KLUDGE!
- * We need these to not be extern in libGL.so because of
- * PR toolchain/50277
- */
-#if defined(GLX_USE_TLS) && defined(__NetBSD__)
-_X_EXPORT __thread struct _glapi_table * _glapi_tls_Dispatch
-    __attribute__((tls_model("initial-exec"))) = NULL;
-_X_EXPORT __thread void * _glapi_tls_Context
-    __attribute__((tls_model("initial-exec")));
-#endif
-
-/*
 ** We setup some dummy structures here so that the API can be used
 ** even if no context is current.
 */
