0499e58f6c7410476ded0ba357299a044323f7e5
mrg <mrg@NetBSD.org>
2024-07-04 19:48:22 +0000

    initial import of fontconfig-2.15.0

--- external/mit/fontconfig/dist/src/fcstdint.h
+++ external/mit/fontconfig/dist/src/fcstdint.h
@@ -1,8 +1,8 @@
 #ifndef _FONTCONFIG_SRC_FCSTDINT_H
 #define _FONTCONFIG_SRC_FCSTDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "fontconfig 2.14.2"
-/* generated using gnu compiler gcc (GCC) 13.0.1 20230117 (Red Hat 13.0.1-0) */
+#define _GENERATED_STDINT_H "fontconfig 2.15.0"
+/* generated using gnu compiler gcc (GCC) 13.2.1 20231110 (Red Hat 13.2.1-5) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
