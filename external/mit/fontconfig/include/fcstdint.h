b5b73dd3ce839577ee3ceb3bec74b479fdb33d9e
mrg <mrg@NetBSD.org>
2024-07-04 19:53:34 +0000

    add new generated files.

---
+++ external/mit/fontconfig/include/fcstdint.h
@@ -0,0 +1,9 @@
+#ifndef _FONTCONFIG_SRC_FCSTDINT_H
+#define _FONTCONFIG_SRC_FCSTDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "fontconfig 2.15.0"
+/* generated using gnu compiler gcc (nb1 20230729) 12.3.0 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
