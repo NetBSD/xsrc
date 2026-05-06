12875c3e33c91bd2bb750145612494c38522ba93
martin <martin@NetBSD.org>
2026-05-06 18:04:25 +0000

    Pull up the following, requested by mrg in ticket #258:
    	xsrc/external/mit/xorg-server/dist/SECURITY.md       up to 1.1.1.1
    	xsrc/external/mit/xorg-server/dist/COPYING           up to 1.1.1.8
    	xsrc/external/mit/xorg-server/dist/ChangeLog         up to 1.1.1.32
    	xsrc/external/mit/xorg-server/dist/Makefile.am       up to 1.1.1.9
    	xsrc/external/mit/xorg-server/dist/Makefile.in       up to 1.1.1.20
    	xsrc/external/mit/xorg-server/dist/aclocal.m4        up to 1.1.1.25
    	xsrc/external/mit/xorg-server/dist/config.guess      up to 1.1.1.19
    	xsrc/external/mit/xorg-server/dist/config.sub        up to 1.1.1.19
    	xsrc/external/mit/xorg-server/dist/configure         up to 1.25
    	xsrc/external/mit/xorg-server/dist/configure.ac      up to 1.27
    	xsrc/external/mit/xorg-server/dist/meson.build       up to 1.1.1.22
    	xsrc/external/mit/xorg-server/dist/Xext/xf86bigfont.c up to 1.3
    	xsrc/external/mit/xorg-server/dist/dix/dixfonts.c    up to 1.8
    	xsrc/external/mit/xorg-server/dist/dix/events.c      up to 1.10
    	xsrc/external/mit/xorg-server/dist/dix/gc.c          up to 1.1.1.9
    	xsrc/external/mit/xorg-server/dist/glamor/glamor.c   up to 1.1.1.10
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_core.c up to 1.1.1.4
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_dash.c up to 1.1.1.4
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_egl.c up to 1.1.1.8
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_largepixmap.c up to 1.1.1.4
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_render.c up to 1.1.1.6
    	xsrc/external/mit/xorg-server/dist/hw/xfree86/loader/loadmod.c up to 1.1.1.10
    	xsrc/external/mit/xorg-server/dist/hw/xfree86/modes/xf86Crtc.h up to 1.3
    	xsrc/external/mit/xorg-server/dist/hw/xfree86/os-support/linux/lnx_video.c up to 1.1.1.10
    	xsrc/external/mit/xorg-server/dist/hw/xquartz/bundle/Info.plist.cpp up to 1.1.1.10
    	xsrc/external/mit/xorg-server/dist/include/do-not-use-config.h.in up to 1.1.1.18
    	xsrc/external/mit/xorg-server/dist/miext/rootless/rootlessCommon.h up to 1.1.1.6
    	xsrc/external/mit/xorg-server/dist/miext/rootless/rootlessScreen.c up to 1.1.1.10
    	xsrc/external/mit/xorg-server/dist/miext/sync/misync.c up to 1.1.1.5
    	xsrc/external/mit/xorg-server/dist/os/connection.c   up to 1.10
    	xsrc/external/mit/xorg-server/dist/os/ospoll.c       up to 1.1.1.3
    	xsrc/external/mit/xorg-server/dist/os/utils.c        up to 1.15
    	xsrc/external/mit/xorg-server/dist/os/xsha1.c        up to 1.1.1.5
    	xsrc/external/mit/xorg-server/dist/randr/rroutput.c  up to 1.1.1.7
    	xsrc/external/mit/xorg-server/dist/render/picture.c  up to 1.1.1.11
    	xsrc/external/mit/xorg-server/dist/xkb/xkb.c         up to 1.13
    	xsrc/external/mit/xorg-server/dist/xkb/xkbActions.c  up to 1.1.1.8
    	xsrc/external/mit/xorg-server/dist/xkb/xkbUtils.c    up to 1.1.1.10
    	xsrc/external/mit/xorg-server/include/dix-config.h   up to 1.58
    	xsrc/external/mit/xorg-server/include/version-config.h up to 1.28
    	xsrc/external/mit/xorg-server/include/xorg-config.h  up to 1.38
    	xsrc/external/mit/xorg-server/include/xorg-server.h  up to 1.38
    
    Apply upstream xorg-server fixes for these CVE/ZDI-CANs:
       CVE-2026-33999, ZDI-CAN-28593
       CVE-2026-34000, ZDI-CAN-28679
       CVE-2026-34001, ZDI-CAN-28706
       CVE-2026-34002, ZDI-CAN-28737
       CVE-2026-34003, ZDI-CAN-28736

--- external/mit/xorg-server/dist/os/utils.c
+++ external/mit/xorg-server/dist/os/utils.c
@@ -1608,7 +1608,9 @@ Fopen(const char *file, const char *type)
     iop = fopen(file, type);
 
     if (seteuid(euid) == -1) {
-        fclose(iop);
+        if (iop) {
+            fclose(iop);
+        }
         return NULL;
     }
     return iop;
@@ -2101,12 +2103,14 @@ xstrtokenize(const char *str, const char *separators)
 void
 FormatInt64(int64_t num, char *string)
 {
+    uint64_t unum = num;
+
     if (num < 0) {
         string[0] = '-';
-        num *= -1;
+        unum = num * -1;
         string++;
     }
-    FormatUInt64(num, string);
+    FormatUInt64(unum, string);
 }
 
 /* Format a number into a string in a signal safe manner. The string should be
