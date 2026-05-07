b25ab61f1614e60824e24ce9de4a573c2ae4c92f
martin <martin@NetBSD.org>
2026-05-07 14:09:20 +0000

    Pull up following revision(s) (requested by hans in ticket #1260):
    	xsrc/external/mit/xf86-input-keyboard/dist/src/bsd_KbdMap.c: revision 1.16
    	xsrc/external/mit/xf86-input-keyboard/dist/src/bsd_kbd.c: revision 1.23
    
    xf86-input-keyboard: add DEC LK401 keyboard map and WSKBD_TYPE_LK401

--- external/mit/xf86-input-keyboard/dist/src/bsd_kbd.c
+++ external/mit/xf86-input-keyboard/dist/src/bsd_kbd.c
@@ -471,6 +471,11 @@ OpenKeyboard(InputInfoPtr pInfo)
                printWsType("LK201", pInfo->name);
                break;
 #endif
+#ifdef WSKBD_TYPE_LK401
+           case WSKBD_TYPE_LK401:
+               printWsType("LK401", pInfo->name);
+               break;
+#endif
 #ifdef WSKBD_TYPE_MAPLE
            case WSKBD_TYPE_MAPLE:
                printWsType("Maple", pInfo->name);
