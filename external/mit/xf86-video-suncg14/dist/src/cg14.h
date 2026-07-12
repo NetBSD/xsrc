bd6fec85fff7da625d9b32b7f7ed44bd221d93f0
mrg <mrg@NetBSD.org>
2026-07-12 02:03:13 +0000

    initial import of xf86-video-suncg14-1.2.0

--- external/mit/xf86-video-suncg14/dist/src/cg14.h
+++ external/mit/xf86-video-suncg14/dist/src/cg14.h
@@ -66,6 +66,12 @@ typedef struct {
  * This should match corresponding definition in Solaris's
  * '/usr/include/sys/cg14io.h'.
  */
+#ifdef __NetBSD__
+#include <dev/sun/fbio.h>
+#include <sys/ioccom.h>
+#define CG14_SET_PIXELMODE	_IOW('M', 3, int)
+#else
 #define CG14_SET_PIXELMODE	(('M' << 8) | 3)
+#endif
 
 #endif /* CG14_H */
