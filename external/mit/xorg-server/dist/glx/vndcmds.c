6b5ff42dd5a8c1814c7db4343ed30b8e955dfcbc
martin <martin@NetBSD.org>
2026-07-19 11:27:07 +0000

    Apply patch, requested by mrg in ticket #2037:
    	xsrc/external/mit/libXfont2/dist/src/bitmap/bitscale.c
    	xsrc/external/mit/libXfont2/dist/src/bitmap/pcfread.c
    	xsrc/external/mit/xorg-server/dist/fb/fbglyph.c
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_font.c
    	xsrc/external/mit/xorg-server/dist/glamor/glamor_glyphblt.c
    	xsrc/external/mit/xorg-server/dist/glx/vndcmds.c
    	xsrc/external/mit/xorg-server/dist/mi/miglblt.c
    
    Apply upstream fixes for CVE-2026-56003/ZDI-CAN-30560,
    CVE-2026-56002/ZDI-CAN-30559, CVE-2026-56001/ZDI-CAN-30558,
    CVE-2026-56000/ZDI-CAN-30561, CVE-2026-55999/ZDI-CAN-30498.

--- external/mit/xorg-server/dist/glx/vndcmds.c
+++ external/mit/xorg-server/dist/glx/vndcmds.c
@@ -162,9 +162,6 @@ static int CommonLoseCurrent(ClientPtr client, GlxContextTagInfo *tagInfo)
             tagInfo->tag, // No old context tag,
             None, None, None, 0);
 
-    if (ret == Success) {
-        GlxFreeContextTag(tagInfo);
-    }
     return ret;
 }
 
@@ -256,6 +253,10 @@ static int CommonMakeCurrent(ClientPtr client,
             if (ret != Success) {
                 return ret;
             }
+            // Free the old tag before calling CommonMakeNewCurrent(),
+            // which may call GlxAllocContextTag() and realloc the
+            // contextTags array, invalidating the oldTag pointer.
+            GlxFreeContextTag(oldTag);
             oldTag = NULL;
         }
 
